b397a90cc3ab7b0ed42bb8f9033df7d049047a1e
sarbj <web2it@hotmail.co.uk>
2016-09-25 19:05:26 +0100

    views

---
+++ Msr.Database/dbo/Views/Portal/Portal_UserView.sql
@@ -0,0 +1,30 @@
+﻿CREATE VIEW [dbo].[Portal_UserView]
+AS
+
+SELECT
+ u.[Id]
+,u.[Email]
+,u.[EmailConfirmed]
+,u.[PasswordHash]
+,u.[SecurityStamp]
+,u.[PhoneNumber] AS Phone
+,u.Phone2
+,u.[PhoneNumberConfirmed]
+,u.[TwoFactorEnabled]
+,u.[LockoutEndDateUtc]
+,u.[LockoutEnabled]
+,u.[AccessFailedCount]
+,u.[UserName]
+,u.[FirstName]
+,u.[LastName]
+,u.[FirstName]+ ' ' +u.[LastName] AS FullName
+,u.[IsActive]
+,u.[CreatedDate]
+,u.[TimeZone]
+,r.Id AS RoleId
+FROM [AspNetUsers] u 
+LEFT JOIN AspNetUserRoles ur ON ur.UserId = u.Id
+LEFT JOIN AspNetRoles r ON r.Id = ur.RoleId
+GO
+
+
