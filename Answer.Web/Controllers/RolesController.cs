e9f08dd59172bb8da8e0d6428f6c64312e9f5ae4
sarb <web2it@hotmail.co.uk>
2017-08-08 17:57:36 +0100

    null ids

--- Answer.Web/Controllers/RolesController.cs
+++ Answer.Web/Controllers/RolesController.cs
@@ -26,7 +26,9 @@ namespace Answer.Web.Controllers
 
             var defaultStatusList = new [] { "CREATING", "DENIED", "APPROVED", "APPROVED_BUT_REVISING", "APPROVED_BUT_DELETING" };
 
-            var totalRows = roleService.GetUserRolesQueryable().Where(x=> defaultStatusList.Contains(x.Status));
+            var totalRows = roleService.GetUserRolesQueryable();
+
+            totalRows = totalRows.Where(x => defaultStatusList.Contains(x.Status) && x.Id.Length > 0);
 
             if (param.where != null && param.where.rules.Any())
             {
