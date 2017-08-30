cd11a55e4e07ed98f66a5f921603287eeabfd726
sarb <web2it@hotmail.co.uk>
2017-08-30 20:36:25 +0100

    Utilities - Parts Received by Customer - Implement from Answer 1.0 #67

---
+++ Msr.Models/Delivery/DeliveryScreenView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Msr.Models.Delivery
+{
+    public class DeliveryScreenView
+    {
+        public string TASK_ID { get; set; }
+        public string PURCHASE_ITEM_ID { get; set; }
+        public string CUST_PURCH_NUM { get; set; }
+        public string COMPANY_PART_NUMBER { get; set; }
+        public string CUST_NAME { get; set; }
+        public string PART_DESC { get; set; }
+        public string PRODUCT_NAME { get; set; }
+        public string PROCEDURE_NAME { get; set; }
+        public string LOCATION_NAME { get; set; }
+        public DateTime? DUE_DATE { get; set; }
+    }
+}
---
+++ Msr.Services/Delivery/DeliveryService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Msr.Models.Delivery;
+using Msr.Repositories;
+
+namespace Msr.Services.Delivery
+{
+    public class DeliveryService
+    {
+        private readonly MsrDbContext _dbContext;
+
+        public DeliveryService()
+        {
+            _dbContext = new MsrDbContext();
+
+        }
+
+        public IQueryable<DeliveryScreenView> GetDeliveryScreenDataToView(string loginId)
+        {
+            var sql = $@"SELECT DISTINCT t.TASK_ID as TaskId,t.PURCHASE_ITEM_ID as PurchaseItemId,t.CUST_PURCH_NUM as CustPurchNum,t.COMPANY_PART_NUMBER as PartNumber,t.CUST_NAME as CustName,t.PART_DESC as PartName,t.PRODUCT_NAME as ProductName,t.LOCATION_NAME as LocationName, t.DUE_DATE FROM A_V_SHIPPER_TASKS t
+                WHERE SYSTEM_TASK = 'SYS_RECEIVE' AND (  (REQUESTEE_ID IS NULL AND GROUP_REQUESTEE_ID IN ('','1502','1490','1498','1506','1510','1494')) OR  REQUESTEE_ID = '{loginId}' ) 
+                UNION SELECT DISTINCT t.TASK_ID as TaskId,t.PURCHASE_ITEM_ID as PurchaseItemId,t.CUST_PURCH_NUM as CustPurchNum,t.COMPANY_PART_NUMBER as PartNumber,t.CUST_NAME as CustName,t.PART_DESC as PartName,t.PRODUCT_NAME as ProductName,t.LOCATION_NAME as LocationName, t.DUE_DATE FROM A_V_SHIPPING_TASK_FROM_BATCH  t WHERE SYSTEM_TASK = 'SYS_RECEIVE' AND
+                ((REQUESTEE_ID IS NULL AND GROUP_REQUESTEE_ID IN ('','1502','1490','1498','1506','1510','1494')) OR  REQUESTEE_ID = '{loginId}' ) ORDER BY DUE_DATE";
+
+            var result = _dbContext.Database.SqlQuery<DeliveryScreenView>(sql).ToList().AsQueryable();
+
+            return result;
+
+
+            ////data = new List<DeliveryScreenViewModel>
+            ////{
+            ////    new DeliveryScreenViewModel
+            ////    {
+            ////        TaskId = "1",
+            ////        CustName = "ss",
+            ////        CustPurchNum = "100",
+            ////        LocationName = "abc",
+            ////        PartName = "text",
+            ////        PartNumber = "200",
+            ////        ProcedureName = "Pro",
+            ////        ProductName = "Product",
+            ////        PurchaseItemId = "300"
+            ////    },
+            ////    new DeliveryScreenViewModel
+            ////    {
+            ////        TaskId = "2",
+            ////        CustName = "ss",
+            ////        CustPurchNum = "100",
+            ////        LocationName = "abc",
+            ////        PartName = "text",
+            ////        PartNumber = "200",
+            ////        ProcedureName = "Pro",
+            ////        ProductName = "Product",
+            ////        PurchaseItemId = "300"
+            ////    }
+
+            ////};
+        }
+    }
+}
