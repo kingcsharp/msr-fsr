4ef781c9e5cd353c07e10ba8fd8cf7e6024673cf
sarb <web2it@hotmail.co.uk>
2017-12-06 00:52:36 +0000

    Pricing - Start, Edit, and Send to Workflow #97

--- Msr.Database/dbo/Tables/Portal_RequirementSteps.sql
+++ Msr.Database/dbo/Tables/Portal_RequirementSteps.sql
@@ -1,19 +1,29 @@
-﻿CREATE TABLE [dbo].[Portal_RequirementSteps] (
-    [Id]                         INT             IDENTITY (1, 1) NOT NULL,
-    [ObjectId]                   NVARCHAR (50)   NOT NULL,
-    [Process]                    NVARCHAR (50)   NULL,
-    [Step]                       INT             NULL,
-    [StandardDirectLaborMinutes] DECIMAL (10, 2) NOT NULL,
-    [StandardMachineMinutes]     DECIMAL (10, 2) NOT NULL,
-    [ReplacementCost]            DECIMAL (18, 2) NULL,
-    [Utilization]                DECIMAL (5, 2)  NULL,
-    [UsefulLife]                 DECIMAL (18, 2) NULL,
-    [EquipExpensePerMinute]      DECIMAL (18, 2) NULL,
-    [AnnualRM]                   DECIMAL (18, 2) NULL,
-    [RMPerMinute]                DECIMAL (18, 2) NULL,
-    CONSTRAINT [PK_Portal_RequirementSteps] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Portal_RequirementSteps_Portal_RequirementSteps] FOREIGN KEY ([ObjectId]) REFERENCES [dbo].[Portal_CustomerRequirement] ([Id])
-);
+﻿CREATE TABLE [dbo].[Portal_RequirementSteps](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerSubmittedRequirementId] [int] NOT NULL,
+	[ObjectId] [nvarchar](50) NOT NULL,
+	[Process] [nvarchar](50) NULL,
+	[Step] [int] NULL,
+	[StandardDirectLaborMinutes] [decimal](10, 2) NOT NULL,
+	[StandardMachineMinutes] [decimal](10, 2) NOT NULL,
+	[ReplacementCost] [decimal](18, 2) NULL,
+	[Utilization] [decimal](5, 2) NULL,
+	[UsefulLife] [decimal](18, 2) NULL,
+	[EquipExpensePerMinute] [decimal](18, 2) NULL,
+	[AnnualRM] [decimal](18, 2) NULL,
+	[RMPerMinute] [decimal](18, 2) NULL,
+ CONSTRAINT [PK_Portal_RequirementSteps] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 
+GO
 
+ALTER TABLE [dbo].[Portal_RequirementSteps]  WITH CHECK ADD  CONSTRAINT [FK_Portal_RequirementSteps_Portal_CustomerSubmittedRequirement] FOREIGN KEY([CustomerSubmittedRequirementId])
+REFERENCES [dbo].[Portal_CustomerSubmittedRequirement] ([Id])
+GO
+
+ALTER TABLE [dbo].[Portal_RequirementSteps] CHECK CONSTRAINT [FK_Portal_RequirementSteps_Portal_CustomerSubmittedRequirement]
+GO
 
