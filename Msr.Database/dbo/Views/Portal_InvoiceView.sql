0e79efcc4359c24d3fe6767635fee434416e2d75
sarb <web2it@hotmail.co.uk>
2018-01-14 15:34:17 +0000

    Invoice IFF Export #124

---
+++ Msr.Database/dbo/Views/Portal_InvoiceView.sql
@@ -0,0 +1,21 @@
+﻿create view [dbo].[Portal_InvoiceView]
+AS
+
+SELECT  (LEFT( p.InvoiceClass,2)) +'-'+RIGHT(YEAR(p.InvoiceDate),2) +'-'+ REPLACE(STR(p.Id, 5), SPACE(1), '0')  as InvoiceNumber,
+p.Id,p.Client,
+p.Description,
+p.Status,
+p.CustPo,
+p.InvoiceDate,
+p.TotalDue,
+p.Tax,
+p.PaymentsAndCredits,
+p.Debits,
+p.LateFees,
+p.Items,
+p.Supplier,
+p.InvoiceClass
+FROM 
+Portal_Invoice AS p
+
+GO
