95de469cb926e0573577fc55f579741a899966ed
sarb <web2it@hotmail.co.uk>
2018-01-24 08:22:13 +0000

    Delivery Ticket Barcode Issue #125

--- Msr.Database/dbo/Stored Procedures/Portal/Portal_GetDeliveryTsrDetails.sql
+++ Msr.Database/dbo/Stored Procedures/Portal/Portal_GetDeliveryTsrDetails.sql
@@ -13,6 +13,6 @@ AS
 	SELECT OBJECT_ID AS PurchaseNumber, ROOT_CO_NAME AS ShipFrom, CUST_PURCH_NUM AS CustomerPo, CONVERT(VARCHAR(10), @getDate, 101) + ' ' 
        + LTRIM(RIGHT(CONVERT(CHAR(20), @getDate, 22), 11)) AS ShipDate FROM A_V_PURCHASES_WITH_SUPPLIER_QUOTES  WHERE PURCH_HIST_ID = @purchaseHistoryId
 	UPDATE A_ORDER_ITEMS SET SHIP_DATE = @getDate WHERE ID = @purchaseItemId;
-	SELECT ca.NAME AS CompanyName, CONCAT(dt.PROD_NAME, '-', dt.PROC_NAME) AS Description, dt.PURCHASE_QTY As Quantity, dt.PURCH_ITEM_ID AS CustomerWoItemNumber, dt.COMPANY_PART_NUMBER AS CompanyPartNumber, 
+	SELECT ca.NAME AS CompanyName, CONCAT(dt.PROD_NAME, '-', dt.PROC_NAME) AS Description,dt.CUST_LINE_ITEM AS CustLineItem, dt.PURCHASE_QTY As Quantity, dt.PURCH_ITEM_ID AS CustomerWoItemNumber, dt.COMPANY_PART_NUMBER AS CompanyPartNumber, 
 	dt.SERIAL AS Serial FROM A_V_DELIVERY_TICKET_PURCH_ITEM_INFO dt JOIN A_V_COMPANIES_APPROVED_DATA_QUICK ca ON dt.ACCT_CUSTOMER = ca.ID WHERE dt.PURCHASE_HIST_ID = @purchaseHistoryId 
 	AND dt.PURCH_ITEM_ID = @purchaseItemId
--- Msr.Services/Orders/Procedures/PurchaseItemInfoResult.cs
+++ Msr.Services/Orders/Procedures/PurchaseItemInfoResult.cs
@@ -4,6 +4,7 @@
     {
         public string CompanyName { get; set; }
         public string Description { get; set; }
+        public string CustLineItem { get; set; }
         public float Quantity { get; set; }
         public string CustomerWoItemNumber { get; set; }
         public string CompanyPartNumber { get; set; }
