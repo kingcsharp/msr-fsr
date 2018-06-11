0560c4e3197cbe03459ed7ff7488cea86d2dd448
web2it <web2it@hotmail.co.uk>
2018-06-11 14:07:18 +0100

    Update SQL view

--- Msr.Database/dbo/Views/Portal_CustomerRequirementView.sql
+++ Msr.Database/dbo/Views/Portal_CustomerRequirementView.sql
@@ -32,10 +32,15 @@ p.REV AS Rev,
 p.IsProduct,
 p.STATUS,
 p.OBJECT_ID AS ObjectId,
-PQP.CUST_ID as CustomerId
+pqp.CUST_ID as CustomerId,
+Customer.NAME AS CustomerName
 FROM  A_V_PRODUCT_SEARCH_DATA p
 INNER JOIN [dbo].[Portal_CustomerSubmittedRequirement] c ON c.Id = p.CustomerRequirementId
 LEFT JOIN Portal_ProceduresView pv ON p.PROCEDURE_ID = pv.ObjectId
 LEFT  JOIN A_V_COMPANIES_DROP_SEARCH sup on sup.ID = p.SUPPLIER_ID
-LEFT JOIN A_PRODUCTS_QUICK_PRICE PQP ON P.ID = PQP.PROD_HIST_ID
+LEFT JOIN A_PRODUCTS_QUICK_PRICE pqp ON P.ID = pqp.PROD_HIST_ID
+OUTER APPLY (
+SELECT cus.Name FROM A_V_COMPANIES_DROP_SEARCH cus
+WHERE cus.Id= pqp.CUST_ID
+) AS Customer
 GO
