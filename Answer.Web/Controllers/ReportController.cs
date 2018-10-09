a53753b06c95c3df138425b6596fc52aa6ec0819
alecellis1985 <alecellis1985@gmail.com>
2018-10-09 20:52:00 -0300

    made proj load with only get data errors

---
+++ Answer.Web/Controllers/ReportController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Msr.Models.Orders;
+using Msr.Services.jqGrid;
+using Msr.Services.Orders;
+using Msr.Services.Users;
+using Answer.Web.Controllers;
+using Answer.Web.Filters;
+
+namespace Msr.Web.Controllers
+{
+    [AuthorizeUser]
+    public class ReportController : BaseController
+    {
+        //[HttpGet,Route("reports/{reportID}")]
+        public ActionResult Index()
+        {
+            ViewBag.ActiveClass = "WIP";
+
+            var loggedUser = User.Identity.GetUserId();
+
+            var userService = new UserService();
+
+            //var company = userService.GetCompanyId(loggedUser);
+            
+            //ViewBag.ClientName = company.Name;
+
+            return View();
+        }
+
+    }
+}
