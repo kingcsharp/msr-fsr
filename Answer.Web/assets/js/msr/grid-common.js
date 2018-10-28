cab5f066dc77e2ab3e218d8bfc6557c451849948
web2it <web2it@hotmail.co.uk>
2018-10-28 13:54:53 +0000

    #266 Add delay when loading dropdown

--- Answer.Web/assets/js/msr/grid-common.js
+++ Answer.Web/assets/js/msr/grid-common.js
@@ -342,7 +342,7 @@ Msr.JqGridCommon = Msr.JqGridCommon ||
                 marginBottom: "1px",
                 paddingTop: "3px"
             });
-        }, 1000);
+        }, 1500);
     },
     GetColumnIndexByName: function (columnName) {
         var cm = $(this).jqGrid('getGridParam', 'colModel'), i, l = cm.length;
