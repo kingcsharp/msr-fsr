03e2d20dc716dacaf8b987ecaf2c4a310b6eb3e2
web2it <web2it@hotmail.co.uk>
2018-11-17 19:41:37 +0000

    #296 Ad-Hoc Reports

---
+++ Msr.Database/dbo/Views/Reports/Report_SerialNumberHistory.sql
@@ -0,0 +1,27 @@
+﻿CREATE VIEW Report_SerialNumberHistory
+AS
+
+SELECT DISTINCT
+pwo.Serial AS SerialNumber,
+pwo.CustPurchNum AS WONumber,
+pwo.StartDate AS WOCreationDate,
+pwo.DueDate AS DueDate,
+pwo.OrigDueDate AS ShipDate, 
+pwo.LocationName AS MSRFSRFacility,
+pwo.CustomerName,
+pwo.ProcName AS specno,
+pwo.WoItem AS KitName,
+'Not sure where to get' AS NCRNumber,
+pwo.ReferencePo AS pono,
+pwo.CustMttn AS mttn,
+PTLCount.CycleCount
+FROM dbo.Portal_WorkOrders pwo 
+INNER JOIN PartsTransactionLog AS pt ON pt.PartId = pwo.PartId AND pt.SerialNumber=pwo.Serial
+OUTER APPLY(
+			SELECT count(SerialNumber) AS CycleCount 
+			FROM PartsTransactionLog AS pt
+			WHERE pt.PartId = pwo.PartId AND pt.SerialNumber=pwo.Serial
+			) PTLCount
+ORDER BY pwo.Serial
+
+GO
