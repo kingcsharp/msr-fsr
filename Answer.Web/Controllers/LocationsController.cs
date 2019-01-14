92c0dcbcb86f4f41b5f07bce8817e6317f261062
alecellis1985 <alecellis1985@gmail.com>
2019-01-14 15:48:08 -0300

    Locations Create using parent info

--- Answer.Web/Controllers/LocationsController.cs
+++ Answer.Web/Controllers/LocationsController.cs
@@ -174,6 +174,33 @@ namespace Answer.Web.Controllers
                     return View(model);
                 }
             }
+            else
+            {
+                if (model.Parent != "" && ModelState["InternalAddress"].Errors.Count == 0 && ModelState["Name"].Errors.Count == 0)
+                {
+                    model.LoggedUserIdResult = GetCurrentUser();
+                    var locationData = _locationService.GetById(model.Parent);
+                    model.Address1 = locationData.Address1;
+                    model.Address2 = locationData.Address2;
+                    model.City = locationData.City;
+                    model.Country = locationData.Country;
+                    model.PostalCode = locationData.PostalCode;
+                    model.Region = locationData.Region;
+                    model.State = locationData.State;
+                    var response = _locationService.Save(model);
+
+                    if (response)
+                    {
+                        TempData["SuccessMessage"] = "Location has been updated successfully.";
+
+                        return RedirectToAction("Index");
+                    }
+
+                    TempData["ErrorMessage"] = "Something went wrong.";
+
+                    return View(model);
+                }
+            }
 
             return View(model);
 
@@ -213,6 +240,36 @@ namespace Answer.Web.Controllers
                 TempData["ErrorMessage"] = "Something went wrong.";
 
                 return View(model);
+            }
+            else
+            {
+                if (model.Parent != "" && ModelState["InternalAddress"].Errors.Count == 0 && ModelState["Name"].Errors.Count == 0)
+                {
+                    model.LoggedUserIdResult = GetCurrentUser();
+
+                    var locationData = _locationService.GetById(model.Parent);
+
+                    model.Address1 = locationData.Address1;
+                    model.Address2 = locationData.Address2;
+                    model.City = locationData.City;
+                    model.Country = locationData.Country;
+                    model.PostalCode = locationData.PostalCode;
+                    model.Region = locationData.Region;
+                    model.State = locationData.State;
+
+                    var response = _locationService.Save(model);
+
+                    if (response)
+                    {
+                        TempData["SuccessMessage"] = "Location has been updated successfully.";
+
+                        return RedirectToAction("Index");
+                    }
+
+                    TempData["ErrorMessage"] = "Something went wrong.";
+
+                    return View(model);
+                }
             }
 
             model.Setup(_regionService, _locationService, GetCurrentUser().Id);
