5d3889ff4917e87b7f0ed01694b1bec58d43636e
alecellis1985 <alecellis1985@gmail.com>
2019-01-25 17:13:20 -0300

    remove not approved locations from get locations query

--- Msr.Services/Locations/LocationService.cs
+++ Msr.Services/Locations/LocationService.cs
@@ -139,7 +139,7 @@ namespace Msr.Services.Locations
 
         public List<LocationResult> GetParentLocations()
         {
-            var result = GetLocationsQueryable().Where(x => x.ParentLocation == null)
+            var result = GetLocationsQueryable().Where(x => x.ParentLocation == null && x.Status == "APPROVED")
                 .Select(x => new LocationResult() { Id = x.Id, Name = x.Name, ObJect_Id = x.ObjectId }).ToList();
             return result;
         }
@@ -153,7 +153,7 @@ namespace Msr.Services.Locations
         public List<LocationResult> GetSitesAndRooms()
         {
             var parentLocationIds = GetLocationsQueryable().Where(x => x.ParentLocation == null).Select(x => x.Id);
-            var result = GetLocationsQueryable().Where(x => x.ParentLocation == null || x.ParentLocation !=null && parentLocationIds.Contains(x.ParentLocation))
+            var result = GetLocationsQueryable().Where(x => (x.ParentLocation == null || x.ParentLocation != null && parentLocationIds.Contains(x.ParentLocation)) && x.Status == "APPROVED")
                 .Select(x => new LocationResult() { Id = x.Id, Name = x.Name, ObJect_Id = x.ObjectId }).ToList();
             return result;
         }
