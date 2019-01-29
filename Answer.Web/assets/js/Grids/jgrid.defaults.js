e5a8d61922480bcdf55f515e30e1387c66552b08
alecellis1985 <alecellis1985@gmail.com>
2019-01-29 13:17:37 -0300

    add missing images of jquery ui, call grid defaults nly if jqgrid is on.

--- Answer.Web/assets/js/Grids/jgrid.defaults.js
+++ Answer.Web/assets/js/Grids/jgrid.defaults.js
@@ -1,16 +1,18 @@
-﻿$.extend($.jgrid.defaults, {
-    ajaxGridOptions: {
-        beforeSend: function (xhr) {
-            console.log('beforeSend');
-            $.blockUI({ message: '<h1><img src="/assets/img/nice_loader.gif" />  Loading. Please wait...</h1>' });
-        },
-        complete: function (xhr) {
-            console.log('complete');
-            $.unblockUI();
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.log('error');
-            $.unblockUI();
+﻿if ($.jgrid && $.jgrid.defaults) {
+    $.extend($.jgrid.defaults, {
+        ajaxGridOptions: {
+            beforeSend: function (xhr) {
+                console.log('beforeSend');
+                $.blockUI({ message: '<h1><img src="/assets/img/nice_loader.gif" />  Loading. Please wait...</h1>' });
+            },
+            complete: function (xhr) {
+                console.log('complete');
+                $.unblockUI();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log('error');
+                $.unblockUI();
+            }
         }
-    }
-});
+    });
+}
