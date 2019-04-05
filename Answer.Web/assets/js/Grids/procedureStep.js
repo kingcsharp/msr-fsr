0fa195d54704623ef422c425f04a89541692ff74
Mike Harvey <mike.harvey@harveyev.com>
2019-04-05 13:33:48 -0400

    Update procedureStep.js
    removed grouped filters

--- Answer.Web/assets/js/Grids/procedureStep.js
+++ Answer.Web/assets/js/Grids/procedureStep.js
@@ -97,7 +97,7 @@ Msr.ProcedureStepGrid = Msr.ProcedureStepGrid ||
                         multiselect: true,
                         searchoptions: {
                             sopt: ['eq'],
-                            value: "CREATING, DENIED, APPROVED, APPROVED_BUT_REVISING, APPROVED_BUT_DELETING:Creating or Approved;CREATING, DENIED:Creating;IN_WORKFLOW:In Approval Workflow;APPROVED, APPROVED_BUT_REVISING, APPROVED_BUT_DELETING:Approved;APPROVED_BUT_REVISING:Approved But Being Revised;APPROVED_BUT_DELETING:Approved But Being Deleted;DENIED:Denied;DELETED:Deleted;OLD:Old",
+                            value: "CREATING: Creating;IN_WORKFLOW:In Approval Workflow;APPROVED:Approved;APPROVED_BUT_REVISING:Approved But Being Revised;APPROVED_BUT_DELETING:Approved But Being Deleted;DENIED:Denied;DELETED:Deleted;OLD:Old",
                             dataInit: function (elem) {
                                 Msr.JqGridCommon.DataInitBootstrapMultiselect(elem);
                             }
