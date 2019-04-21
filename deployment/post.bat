12dcf20f7020ac033df7b2b89dcb6000fa89b28b
Tim Millett <aydensoft@gmail.com>
2019-04-20 20:43:25 -0400

    Adding deploy files

---
+++ deployment/post.bat
@@ -0,0 +1,11 @@
+#ren D:\websites\*.nupkg  Publish.zip
+echo a | d:\tools\7-Zip\7z x D:\websites\publish.zip -oD:\websites\answer2-admin\
+
+echo "Copy Web.config from old to new"
+xcopy /y D:\websites\answer2-admin.old\Web.config D:\websites\answer2-admin\
+
+echo "Start IIS"
+iisreset /start
+
+echo "Delete Publish.zip"
+del "D:\websites\Publish.zip" /s /f /q
