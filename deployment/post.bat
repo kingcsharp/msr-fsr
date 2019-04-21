49b82843fc849bbc7df019fc306b67a4610479cc
Tim Millett <aydensoft@gmail.com>
2019-04-20 22:58:09 -0400

    Update deployment files

--- deployment/post.bat
+++ deployment/post.bat
@@ -1,14 +1,5 @@
-::ren D:\websites\*.nupkg  Publish.zip
-::echo a | d:\tools\7-Zip\7z x D:\websites\publish.zip -oD:\websites\answer2-admin\
-
-::echo "Rename Answer.Web to answer2-admin"
-::ren D:\Answer.Web answer2-admin
-
 echo "Copy Web.config from old to new"
-xcopy /y D:\websites\answer2-admin.old\Web.config D:\websites\answer2-admin\
+xcopy /y D:\MSRFSR\answer2-admin.old\Web.config D:\MSRFSR\answer2-admin\
 
 echo "Start IIS"
 iisreset /start
-
-::echo "Delete Publish.zip"
-::del "D:\websites\Publish.zip" /s /f /q
--- deployment/pre.bat
+++ deployment/pre.bat
@@ -1,15 +1,11 @@
-::set folder="D:\codedeploy"
-::cd /d %folder%
-::for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
-
 echo "Shutdown IIS"
 iisreset /stop
 
 echo "Deleting old backup"
-rmdir /s /q D:\websites\answer2-admin.old
+rmdir /s /q D:\MSRFSR\answer2-admin.old
 
 echo "Backing up files"
-ren D:\websites\answer2-admin answer2-admin.old
+ren D:\MSRFSR\answer2-admin answer2-admin.old
 
 echo "Creating new directory
-md D:\websites\answer2-admin
+md D:\MSRFSR\answer2-admin
