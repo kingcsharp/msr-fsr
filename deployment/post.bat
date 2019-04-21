dcd6e7cb0487cf0c70c51891dbcfa86c4e40ce0e
Tim Millett <aydensoft@gmail.com>
2019-04-20 22:33:14 -0400

    Update

--- deployment/post.bat
+++ deployment/post.bat
@@ -1,5 +1,5 @@
 ::ren D:\websites\*.nupkg  Publish.zip
-echo a | d:\tools\7-Zip\7z x D:\websites\publish.zip -oD:\websites\answer2-admin\
+::echo a | d:\tools\7-Zip\7z x D:\websites\publish.zip -oD:\websites\answer2-admin\
 
 echo "Rename Answer.Web to answer2-admin"
 ren D:\Answer.Web answer2-admin
@@ -10,5 +10,5 @@ xcopy /y D:\websites\answer2-admin.old\Web.config D:\websites\answer2-admin\
 echo "Start IIS"
 iisreset /start
 
-echo "Delete Publish.zip"
-del "D:\websites\Publish.zip" /s /f /q
+::echo "Delete Publish.zip"
+::del "D:\websites\Publish.zip" /s /f /q
