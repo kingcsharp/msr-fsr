f60a6bdd2e4c57adbfa0e15e8b30af331eeb29aa
Tim Millett <aydensoft@gmail.com>
2019-04-30 18:44:44 -0400

    Fix deployment

--- deployment/post.bat
+++ deployment/post.bat
@@ -1,5 +1,11 @@
 echo "Copy Web.config from old to new"
 xcopy /y D:\MSRFSR\answer2-admin.old\Web.config D:\MSRFSR\answer2-admin\
 
+echo "Creating roslyn directory
+md D:\MSRFSR\answer2-admin\bin\roslyn
+
+echo "Copy bin\roslyn contents from old to new"
+xcopy /s D:\MSRFSR\answer2-admin_HOLD\bin\roslyn D:\MSRFSR\answer2-admin\bin\roslyn
+
 echo "Start IIS"
 iisreset /start
