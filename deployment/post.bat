ce4bad506cc7be5ca399ac4cbb165155e1f87080
Tim Millett <aydensoft@gmail.com>
2019-05-15 21:28:37 -0400

    Update post deployment script

--- deployment/post.bat
+++ deployment/post.bat
@@ -4,8 +4,8 @@ xcopy /y D:\MSRFSR\answer2-admin.old\Web.config D:\MSRFSR\answer2-admin\
 echo "Creating roslyn directory
 md D:\MSRFSR\answer2-admin\bin\roslyn
 
-echo "Copy bin\roslyn contents from old to new"
-xcopy /s D:\MSRFSR\answer2-admin_HOLD\bin\roslyn D:\MSRFSR\answer2-admin\bin\roslyn
+::echo "Copy bin\roslyn contents from old to new"
+::xcopy /s D:\MSRFSR\answer2-admin_HOLD\bin\roslyn D:\MSRFSR\answer2-admin\bin\roslyn
 
 echo "Start IIS"
 iisreset /start
