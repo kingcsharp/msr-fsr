a8cfdc51be86c744eec09b0bc3d741009678889a
Tim Millett <aydensoft@gmail.com>
2019-05-16 21:25:18 -0400

    Adding roslyn files, update deployment script

--- deployment/post.bat
+++ deployment/post.bat
@@ -1,11 +1,5 @@
 echo "Copy Web.config from old to new"
 xcopy /y D:\MSRFSR\answer2-admin.old\Web.config D:\MSRFSR\answer2-admin\
 
-echo "Creating roslyn directory
-md D:\MSRFSR\answer2-admin\bin\roslyn
-
-echo "Copy bin\roslyn contents from old to new"
-xcopy /s D:\MSRFSR\Roslyn45 D:\MSRFSR\answer2-admin\bin\roslyn
-
 echo "Start IIS"
 iisreset /start
