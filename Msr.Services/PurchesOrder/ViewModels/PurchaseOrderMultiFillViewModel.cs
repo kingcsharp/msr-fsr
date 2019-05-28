f12591e71b44f39f3c9147c1a18807a0e10b8d54
web2it <web2it@hotmail.co.uk>
2019-05-28 22:10:50 +0100

    Answer Purchase Order Detail

--- Msr.Services/PurchesOrder/ViewModels/PurchaseOrderMultiFillViewModel.cs
+++ Msr.Services/PurchesOrder/ViewModels/PurchaseOrderMultiFillViewModel.cs
@@ -56,7 +56,7 @@ namespace Msr.Services.PurchesOrder.ViewModels
                             PRICING_TABLE_ID = item.PRICING_TABLE_ID,
                             CUSTOMER = item.CUSTOMER,
                             SUPPLIER = item.SUPPLIER,
-                            PURCHASE_QTY = item.PURCHASE_QTY,
+                            PURCHASE_QTY = item.GroupWO ? item.PURCHASE_QTY : 1,
                             UNIT_PRICE = item.UNIT_PRICE,
                             TOTAL_PRICE = item.TOTAL_PRICE,
                             DEST = item.DEST,
@@ -93,8 +93,13 @@ namespace Msr.Services.PurchesOrder.ViewModels
                             LocationList = locationList,
                             OwnerList = ownerList,
                         };
-                        this.FillList.Add(purchaseOrderFillViewModel);
+                    this.FillList.Add(purchaseOrderFillViewModel);
+
+                    if (item.GroupWO)
+                    {
+                        break;
                     }
+                    } 
                     else
                     {
                         item.LocationList = locationList;
