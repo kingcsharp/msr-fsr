b6452e415ddd94025ed86fb7fd2ca3f1b490aab1
Justin Gunther <justin.gunther@nascentvisions.com>
2019-06-15 19:18:00 -0500

    Proper Date Filter On Columns Fix

--- Answer.Web/assets/js/reports/public/js/widgets/grid.js
+++ Answer.Web/assets/js/reports/public/js/widgets/grid.js
@@ -127,7 +127,7 @@
                     var filterElem = filters[filtersLength];
                     
                     if (filterElem.fromDate !== undefined) {
-                        debugger;
+                        // debugger;
                         var d1 = moment(item[filterElem.elementID], 'MMMM-YY')._d;
                         var d2 = moment(filterElem.val, 'M/D/YYYY')._d;
 
@@ -404,18 +404,45 @@ app.service('grid', ['$sce', function ($sce) {
     }
 
     function renderFilter(column, columnIndex, report, reportId) {
-        //5be2442a4f625d000b986c89
         if (report.query.data.length < 1) {
             return smartDropdownFilter(column, columnIndex, reportId);
         }
 
-        var valIsDate = moment(report.query.data[0][column.id]).isValid();
-        if (valIsDate) {
+        // var valIsDate = moment(report.query.data[0][column.id]).isValid();
+        const cellValueIsDate = isValidDate(report.query.data[0][column.id]);
+
+        if (cellValueIsDate === true) {
             return getDateTimeFilter(column, columnIndex, reportId);
         } else {
             return smartDropdownFilter(column, columnIndex, reportId);
         }
+
     }
+
+    function isValidDate(dateString) {
+        // First check for the pattern
+        if (!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString))
+            return false;
+
+        // Parse the date parts to integers
+        var parts = dateString.split("/");
+        var day = parseInt(parts[1], 10);
+        var month = parseInt(parts[0], 10);
+        var year = parseInt(parts[2], 10);
+
+        // Check the ranges of month and year
+        if (year < 1000 || year > 3000 || month == 0 || month > 12)
+            return false;
+
+        var monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+        // Adjust for leap years
+        if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+            monthLength[1] = 29;
+
+        // Check the range of the day
+        return day > 0 && day <= monthLength[month - 1];
+    };
 
     function getDateTimeFilter(column, columnIndex, reportId) {
         var id = "dr_" + new Date().getTime().toString();
