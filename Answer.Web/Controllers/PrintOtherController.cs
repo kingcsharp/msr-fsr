06f292f013ea10f9464c03fc1bb7131d2a8e4124
Mike Harvey <mike.harvey@harveyev.com>
2019-08-26 19:41:39 -0400

    Print Other Labal Roll and Invoice for cancelled Steps Changes

--- Answer.Web/Controllers/PrintOtherController.cs
+++ Answer.Web/Controllers/PrintOtherController.cs
@@ -44,6 +44,10 @@ namespace Answer.Web.Controllers
             {
                 return PartLabelTsr(id);
             }
+            if (reportType == "PART_LABEL_ROLL")
+            {
+                return PartLabelRollTsr(id);
+            }
 
             return Content("Report Type not found");
         }
@@ -86,27 +90,6 @@ namespace Answer.Web.Controllers
             return PartialView("_ViewNcrTsr", ncrDetails);
         }
 
-        //private List<DocumentView> GetDocViewModel(List<DocumentView> docs, OrderService orderService, int width)
-        //{
-        //    var photos = new List<DocumentView>();
-
-        //    foreach (var doc in docs)
-        //    {
-        //        if (doc.ContentType == "image/jpeg" || doc.ContentType == "image/gif" || doc.ContentType == "image/png")
-        //        {
-        //            var photo = orderService.GetDocumentBase64(doc.ServerPath);
-
-        //            photos.Add(new DocumentView
-        //            {
-        //                FileArray = photo,
-        //                Name = doc.Name
-        //            });
-        //        }
-        //    }
-
-        //    return photos;
-        //}
-
         private List<DocumentView> GetDocViewModel(List<DocumentView> docs, OrderService orderService, int width)
         {
             var photos = new List<DocumentView>();
@@ -135,6 +118,12 @@ namespace Answer.Web.Controllers
         {
             var response = _orderService.GetPartLabelTsrDetails(id);
             return PartialView("_ViewPartLabelTsr", response);
+        }
+
+        public ActionResult PartLabelRollTsr(int id)
+        {
+            var response = _orderService.GetPartLabelRollTsrDetails(id);
+            return PartialView("_ViewPartLabelRollTsr", response);
         }
 
         public ActionResult MonitorLabelTsr(int id)
--- Answer.Web/assets/js/msr/grid-common.js
+++ Answer.Web/assets/js/msr/grid-common.js
@@ -392,26 +392,28 @@ Msr.JqGridCommon = Msr.JqGridCommon ||
         },
 
         DataInitBootstrapMultiselect: function (elem, options, callback) {
-            $(document).ready(function () {
-                $(elem).attr('multiple', 'multiple');
-                var multiselectOptions = {
-                    includeSelectAllOption: true,
-                    onInitialized: function ($aSelect, $aContainer) {
-                        var $dropdown = $aContainer.find('.btn'),
-                            offset = $dropdown.offset();
-                        $aContainer.find('.dropdown-menu').css({
-                            position: 'fixed',
-                            top: (offset.top + $dropdown.outerHeight()),
-                            left: offset.left
-                        });
-            
-                        $(window).scroll(function () {
+            setTimeout(function () {
+                $(document).ready(function () {
+                    $(elem).attr('multiple', 'multiple');
+                    var multiselectOptions = {
+                        includeSelectAllOption: true,
+                        onInitialized: function ($aSelect, $aContainer) {
                             var $dropdown = $aContainer.find('.btn'),
                                 offset = $dropdown.offset();
                             $aContainer.find('.dropdown-menu').css({
                                 position: 'fixed',
-                                top: (offset.top + $dropdown.outerHeight() - $(window).scrollTop()),
+                                top: (offset.top + $dropdown.outerHeight()),
                                 left: offset.left
+                            });
+
+                            $(window).scroll(function () {
+                                var $dropdown = $aContainer.find('.btn'),
+                                    offset = $dropdown.offset();
+                                $aContainer.find('.dropdown-menu').css({
+                                    position: 'fixed',
+                                    top: (offset.top + $dropdown.outerHeight() - $(window).scrollTop()),
+                                    left: offset.left
+                                });
                             });
                         },
                         selectAllValue: 'All',
@@ -422,10 +424,13 @@ Msr.JqGridCommon = Msr.JqGridCommon ||
                         },
                         onDropdownHidden: function () {
                             $('.multiselect-update').parent().remove();
+                            $(elem).attr('multiple', 'multiple').change();
                         }
                     };
                     $(elem).multiselect(multiselectOptions);
-            });
+                });
+            }, 100);
+
         },
 
         GetColumnIndexByName: function (columnName) {
