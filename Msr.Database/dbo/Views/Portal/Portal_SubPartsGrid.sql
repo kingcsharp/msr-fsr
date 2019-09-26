8ab31fd0c65d056b307c525edbadad15c02a3f21
David Dombrowsky <davek@6thstreetradio.org>
2019-09-25 22:38:30 -0400

    updated Portal_SubPartsGrid to include CycleCount

--- Msr.Database/dbo/Views/Portal/Portal_SubPartsGrid.sql
+++ Msr.Database/dbo/Views/Portal/Portal_SubPartsGrid.sql
@@ -1,12 +1,24 @@
 ﻿CREATE VIEW [dbo].[Portal_SubPartsGrid]
 AS
 
-SELECT   distinct  subP.COMPANY_PART_NUMBER AS SUB_PART_CO_NUM, subP.NAME AS SUB_PART_NAME, parent.ID AS ROOT, subP.ID AS SUB_PART_ID, 
-                      parent.OBJECT_ID AS PARENT_OBJECT_ID, link.QTY, link.ID, link.NICK_NAME,
-					  p.SerialNumber
-FROM         dbo.A_PARTS_HISTORY parent INNER JOIN
-                      dbo.A_PARTS_SUB_PARTS link ON parent.ID = link.PARENT INNER JOIN
-                      dbo.A_V_PARTS_APPROVED_DATA_QUICK subP ON link.PART_ID = subP.ID INNER join PartsTransactionLog p on p.PartId=parent.Object_Id
+SELECT
+distinct
+subp.COMPANY_PART_NUMBER AS SUB_PART_CO_NUM,
+subp.NAME AS SUB_PART_NAME,
+parent.ID AS ROOT,
+subp.ID AS SUB_PART_ID,
+parent.OBJECT_ID AS PARENT_OBJECT_ID,
+psp.QTY,
+psp.ID,
+psp.NICK_NAME,
+p.SerialNumber,
+psp.part_id,
+wo.CycleCount
+FROM dbo.A_PARTS_HISTORY parent
+INNER JOIN dbo.A_PARTS_SUB_PARTS psp ON parent.ID = psp.PARENT
+INNER JOIN dbo.A_V_PARTS_APPROVED_DATA_QUICK subp ON psp.PART_ID = subp.ID
+INNER JOIN PartsTransactionLog p ON p.PartId=parent.Object_Id
+INNER JOIN Portal_workOrders wo ON subp.ID = wo.partid
 
 GO
 
