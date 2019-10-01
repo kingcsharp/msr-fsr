048720b453e5629c8f80ca8acce21adca3bfe63d
Tim Millett <aydensoft@gmail.com>
2019-10-01 11:23:51 -0400

    Only generate sql script for stage at the moment

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -46,8 +46,12 @@ pipeline {
             steps {
                 script {
                     try {
-                        withCredentials([usernamePassword(credentialsId: '20d94a70-d354-4154-b896-72b12943f904', passwordVariable: 'DB_PASS', usernameVariable: 'DB_USER')]) {
-                            bat label: '', script: 'sqlpackage.exe /a:script /SourceFile:%WORKSPACE%\\Msr.Database\\bin\\Release\\Msr.Database.dacpac /TargetConnectionString:"Data Source=bang.msr-fsr.com;Initial Catalog=${CATALOG_DEV};User Id=${DB_USER};Password=${DB_PASS};Integrated Security=true" /OutputPath:temp.sql'
+                        if(env.JOB_NAME == "MSR-FSR/Answer2.0/Stage") {
+                            withCredentials([usernamePassword(credentialsId: '20d94a70-d354-4154-b896-72b12943f904', passwordVariable: 'DB_PASS', usernameVariable: 'DB_USER')]) {
+                                bat label: '', script: 'sqlpackage.exe /a:script /SourceFile:%WORKSPACE%\\Msr.Database\\bin\\Release\\Msr.Database.dacpac /TargetConnectionString:"Data Source=bang.msr-fsr.com;Initial Catalog=${CATALOG_DEV};User Id=${DB_USER};Password=${DB_PASS};Integrated Security=true" /OutputPath:temp.sql'
+                            }
+                        } else {
+                            echo "Not building script for env.JOB_NAME"
                         }
                     } catch(e) {
                         office365ConnectorSend color: "${RED}", message: "${JOB_NAME} build FAILED generating SQL script from Msr.Database. \n Error: ${e}", status: 'Failed', webhookUrl: "${WEBHOOK_URL}"
