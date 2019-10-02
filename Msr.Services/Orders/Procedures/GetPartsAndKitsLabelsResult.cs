4aedcc7e653f4913a8c81133bad8d192dec2b935
Mike Harvey <mike.harvey@harveyev.com>
2019-10-02 14:07:06 -0400

    Updates for Labels. Counter values not coming back

--- Msr.Services/Orders/Procedures/GetPartsAndKitsLabelsResult.cs
+++ Msr.Services/Orders/Procedures/GetPartsAndKitsLabelsResult.cs
@@ -13,8 +13,6 @@ namespace Msr.Services.Orders.Procedures
 
         public string Actual_Part_ID { get; set; }
 
-        public int Count { get; set; }
-
         public string Site_Name { get; set; }
 
         public string Due_Date { get; set; }
@@ -22,6 +20,8 @@ namespace Msr.Services.Orders.Procedures
         public string WO_Item_Number { get; set; }
 
         public string PO_Number { get; set; }
+
+        public int Cycle_Count { get; set; }
     }
 
 }
