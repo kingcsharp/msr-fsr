548257e6b16191fd3b31a574d078f9271d1537c6
Tim Millett <aydensoft@gmail.com>
2019-10-03 20:34:17 -0400

    Test publish

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -46,13 +46,13 @@ pipeline {
             steps {
                 script {
                     try {
-                        if(env.JOB_NAME == "MSR-FSR/Answer2.0/Stage") {
+                        //if(env.JOB_NAME == "MSR-FSR/Answer2.0/Stage") {
                             withCredentials([usernamePassword(credentialsId: '20d94a70-d354-4154-b896-72b12943f904', passwordVariable: 'DB_PASS', usernameVariable: 'DB_USER')]) {
                                 bat label: '', script: 'sqlpackage.exe /a:script /SourceFile:%WORKSPACE%\\Msr.Database\\bin\\Release\\Msr.Database.dacpac /TargetConnectionString:"Data Source=bang.msr-fsr.com;Initial Catalog=Answer2_Stage;User Id=sa;Password=L8xvg2FGqs7CEQ+s;Integrated Security=true" /OutputPath:temp.sql'
                             }
-                        } else {
-                            echo "Not building script for env.JOB_NAME"
-                        }
+                        //} else {
+                            //echo "Not building script for env.JOB_NAME"
+                        //}
                     } catch(e) {
                         office365ConnectorSend color: "${RED}", message: "${JOB_NAME} build FAILED generating SQL script from Msr.Database. \n Error: ${e}", status: 'Failed', webhookUrl: "${WEBHOOK_URL}"
                     }
@@ -63,15 +63,15 @@ pipeline {
             steps {
                 script {
                     try {
-                        if(env.JOB_NAME == "MSR-FSR/Answer2.0/Stage") {
+                        //if(env.JOB_NAME == "MSR-FSR/Answer2.0/Stage") {
                             //withCredentials([usernamePassword(credentialsId: '20d94a70-d354-4154-b896-72b12943f904', passwordVariable: 'DB_PASS', usernameVariable: 'DB_USER')]) {
                             //bat label: '', script: 'sqlpackage.exe /a:script /SourceFile:%WORKSPACE%\\Msr.Database\\bin\\Release\\Msr.Database.dacpac /TargetConnectionString:"Data Source=bang.msr-fsr.com;Initial Catalog=Answer2_Stage;User Id=sa;Password=L8xvg2FGqs7CEQ+s;Integrated Security=true" /OutputPath:temp.sql'
                             bat label: '', script: 'sqlpackage.exe /a:publish /tdn:Answer2_Stage /SourceFile:%WORKSPACE%\\Msr.Database\\bin\\Release\\Msr.Database.dacpac /TargetConnectionString:"Data Source=bang.msr-fsr.com;Initial Catalog=Answer2_Stage;User Id=sa;Password=L8xvg2FGqs7CEQ+s;Integrated Security=true"'
 
                             //}
-                        } else {
-                            echo "Not building script for env.JOB_NAME"
-                        }
+                        //} else {
+                          //  echo "Not building script for env.JOB_NAME"
+                        //}
                     } catch(e) {
                         office365ConnectorSend color: "${RED}", message: "${JOB_NAME} build FAILED publishing SQL script from Msr.Database. \n Error: ${e}", status: 'Failed', webhookUrl: "${WEBHOOK_URL}"
                     }
