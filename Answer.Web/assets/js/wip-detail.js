cbd49183cd4940f95f98ddb5f4c0d8f1862d5111
Mike Harvey <mike.harvey@harveyev.com>
2019-10-04 09:13:17 -0400

    UI Updates for Print Other

--- Answer.Web/assets/js/wip-detail.js
+++ Answer.Web/assets/js/wip-detail.js
@@ -171,7 +171,7 @@
         });
 
         $('#display-report').on('click', function () {
-            $('#print-other-content').html('<img src="/assets/img/loading.gif"  style="width:32px;height:32px;" />');
+            $('#print-other-content').block({ message: '<h5><img src="/assets/img/nice_loader.gif" />  Loading. Please wait...</h5>' });
             var id = $('.print-other-fill-id').val();
             var reportType = $('#PrintOtherId').val();
 
@@ -191,7 +191,7 @@
                 url: url,
                 dataType: 'html',
                 success: function (data) {
-                    $('#print-other-content').html(data);
+                    $('#print-other-content').unblock().html(data);
                     $('#print-other-report').show();
                     $('#display-report').hide();
                 },
@@ -339,7 +339,12 @@
             $(stepInProgress).removeClass('waiting');
             $(stepInProgress).addClass('requested');
             $(stepInProgress).trigger("click");
-        } else {
+        }
+        // if is complete, dont set currStep MH
+        else if (stepInProgress.length === 0) {
+            loadStep($('#carousel ul.slides li.step').first());
+        }
+        else {
             var step = $('#carousel ul.slides li.step').first();
             if ($(step).data("stepid") === currStepId) {
                 return;
@@ -354,7 +359,6 @@
         var stepId = $(step).data("stepid");
         var phStepId = $(step).data("phstepid");
         var fillId = $(step).data("fill-id");
-        //$('#step-' + stepId).html('<img src="/assets/img/loading.gif"  style="width:32px;height:32px;" />');
         $('#step-' + stepId).empty().html('');
         $('.step-container').block({ message: '<h1><img src="/assets/img/nice_loader.gif" />  Loading. Please wait...</h1>' });
         $.ajax({
