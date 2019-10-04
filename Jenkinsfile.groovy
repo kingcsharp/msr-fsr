a2f8f8263fe7e8f275374f8ee89e7d5aa6025f11
Tim Millett <aydensoft@gmail.com>
2019-10-03 20:35:48 -0400

    Test publish

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -47,9 +47,9 @@ pipeline {
                 script {
                     try {
                         //if(env.JOB_NAME == "MSR-FSR/Answer2.0/Stage") {
-                            withCredentials([usernamePassword(credentialsId: '20d94a70-d354-4154-b896-72b12943f904', passwordVariable: 'DB_PASS', usernameVariable: 'DB_USER')]) {
+                            //withCredentials([usernamePassword(credentialsId: '20d94a70-d354-4154-b896-72b12943f904', passwordVariable: 'DB_PASS', usernameVariable: 'DB_USER')]) {
                                 bat label: '', script: 'sqlpackage.exe /a:script /SourceFile:%WORKSPACE%\\Msr.Database\\bin\\Release\\Msr.Database.dacpac /TargetConnectionString:"Data Source=bang.msr-fsr.com;Initial Catalog=Answer2_Stage;User Id=sa;Password=L8xvg2FGqs7CEQ+s;Integrated Security=true" /OutputPath:temp.sql'
-                            }
+                            //}
                         //} else {
                             //echo "Not building script for env.JOB_NAME"
                         //}
