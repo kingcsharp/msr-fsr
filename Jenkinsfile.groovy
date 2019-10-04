0b97f14c5e97a5d7d662ab0df5b7cb4fb93938e1
Tim Millett <aydensoft@gmail.com>
2019-10-03 20:31:03 -0400

    Test publish

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -59,6 +59,26 @@ pipeline {
                 }
             }
         }
+        stage("Publish SQL Script") {
+            steps {
+                script {
+                    try {
+                        if(env.JOB_NAME == "MSR-FSR/Answer2.0/Stage") {
+                            //withCredentials([usernamePassword(credentialsId: '20d94a70-d354-4154-b896-72b12943f904', passwordVariable: 'DB_PASS', usernameVariable: 'DB_USER')]) {
+                            //bat label: '', script: 'sqlpackage.exe /a:script /SourceFile:%WORKSPACE%\\Msr.Database\\bin\\Release\\Msr.Database.dacpac /TargetConnectionString:"Data Source=bang.msr-fsr.com;Initial Catalog=Answer2_Stage;User Id=sa;Password=L8xvg2FGqs7CEQ+s;Integrated Security=true" /OutputPath:temp.sql'
+                            bat label: '', script: 'sqlpackage.exe /a:publish /tdn:Answer2_Stage /SourceFile:%WORKSPACE%\\Msr.Database\\bin\\Release\\Msr.Database.dacpac /TargetConnectionString:"Data Source=bang.msr-fsr.com;Initial Catalog=Answer2_Stage;User Id=sa;Password=L8xvg2FGqs7CEQ+s;Integrated Security=true"'
+
+                            //}
+                        } else {
+                            echo "Not building script for env.JOB_NAME"
+                        }
+                    } catch(e) {
+                        office365ConnectorSend color: "${RED}", message: "${JOB_NAME} build FAILED publishing SQL script from Msr.Database. \n Error: ${e}", status: 'Failed', webhookUrl: "${WEBHOOK_URL}"
+                    }
+                }
+            }
+        }
+        /*
         stage("Process Transforms") {
             steps {
                 script {
@@ -120,6 +140,7 @@ pipeline {
                 }
             }
         }
+        */
         /*
         stage("Notify Rollbar") {
             agent { label 'master'}
