912a5a511efb3a18167203701ab3400ad9918d02
Mike Harvey <mike@cmhworks.com>
2019-12-05 14:04:08 -0500

    Customer Column Updates

--- Answer.Web/Controllers/PurchaseOrderController.cs
+++ Answer.Web/Controllers/PurchaseOrderController.cs
@@ -95,9 +95,9 @@ namespace Answer.Web.Controllers
                         totalRows = totalRows.Where(x => x.SupplierName.ToLower().Contains(rule.data.ToLower()));
                     }
 
-                    else if (rule.field == nameof(PurchesOrderView.CustomerCo))
+                    else if (rule.field == nameof(PurchesOrderView.CustomerName))
                     {
-                        totalRows = totalRows.Where(x => x.CustomerCo.ToLower().Contains(rule.data.ToLower()));
+                        totalRows = totalRows.Where(x => x.CustomerName.ToLower().Contains(rule.data.ToLower()));
                     }
 
                     else if (rule.field == nameof(PurchesOrderView.OpenDate))
