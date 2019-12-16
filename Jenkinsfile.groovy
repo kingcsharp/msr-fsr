1b0cc13fef04494fb9cdf81850e14602c7a9abb9
Tim Millett <aydensoft@gmail.com>
2019-12-16 18:27:49 -0500

    Update

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -25,7 +25,7 @@ pipeline {
                 script {
                     try {
                         //bat label: '', script: '.nuget\\Nuget.exe install packages.config -o packages'
-                        bat label: '', script: '.nuget\\Nuget.exe restore Answer.Web\packages.config -PackagesDirectory ..\\packages'
+                        bat label: '', script: '.nuget\\Nuget.exe restore Answer.Web\\packages.config -PackagesDirectory ..\\packages'
                     } catch(e) {
                         office365ConnectorSend color: "${RED}", message: "${JOB_NAME} build FAILED installing packages. \n Error: ${e}", status: 'Failed', webhookUrl: "${WEBHOOK_URL}"
                         currentBuild.result = 'FAILURE'
