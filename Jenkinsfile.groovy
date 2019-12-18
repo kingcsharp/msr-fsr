ea034f66f3e7d773be07ab41c0d71a1d275e5e0b
Tim Millett <aydensoft@gmail.com>
2019-12-17 21:06:04 -0500

    Update

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -37,7 +37,11 @@ pipeline {
             steps {
                 script {
                     try {
-                        bat "\"${tool 'v2019'}\" Msr.Database/Msr.Database.sqlproj /t:Build /p:Configuration=Release"
+                        if(env.JOB_NAME == "MSR-FSR/Answer2.0/stage") {
+                            bat "\"${tool 'v2019'}\" Msr.Database/Msr.Database.sqlproj /t:Build /p:Configuration=Release"
+                        } else {
+                            echo "Not building database for ${env.JOB_NAME}"
+                        }
                     } catch(e) {
                         office365ConnectorSend color: "${RED}", message: "${JOB_NAME} build FAILED building Msr.Database. \n Error: ${e}", status: 'Failed', webhookUrl: "${WEBHOOK_URL}"
                         currentBuild.result = 'FAILURE'
@@ -49,16 +53,29 @@ pipeline {
             steps {
                 script {
                     try {
-                        if(env.JOB_NAME == "MSR-FSR/Answer2.0/Stage" || env.JOB_NAME.startsWith("MSR-FSR/Answer2.0/Release/Release")) {
-                            withCredentials([usernamePassword(credentialsId: '20d94a70-d354-4154-b896-72b12943f904', passwordVariable: 'DB_PASS', usernameVariable: 'DB_USER')]) {
-                                bat label: '', script: 'sqlpackage.exe /a:script /SourceFile:%WORKSPACE%\\Msr.Database\\bin\\Release\\Msr.Database.dacpac /TargetConnectionString:"Data Source=bang.msr-fsr.com;Initial Catalog=Answer2_Stage;User Id=sa;Password=L8xvg2FGqs7CEQ+s;Integrated Security=true" /OutputPath:temp.sql'
-                            }
+                        if(env.JOB_NAME == "MSR-FSR/Answer2.0/stage" || env.JOB_NAME.startsWith("MSR-FSR/Answer2.0/Release/Release")) {
+                            bat label: '', script: 'sqlpackage.exe /a:script /SourceFile:%WORKSPACE%\\Msr.Database\\bin\\Release\\Msr.Database.dacpac /TargetConnectionString:"Data Source=bang.msr-fsr.com;Initial Catalog=Answer2_Stage;User Id=sa;Password=L8xvg2FGqs7CEQ+a;Integrated Security=true" /OutputPath:temp.sql'
                         } else {
-                            echo "Not building script for env.JOB_NAME"
+                            echo "Not building script for ${env.JOB_NAME}"
                         }
                     } catch(e) {
                         office365ConnectorSend color: "${RED}", message: "${JOB_NAME} build FAILED generating SQL script from Msr.Database. \n Error: ${e}", status: 'Failed', webhookUrl: "${WEBHOOK_URL}"
                         currentBuild.result = 'FAILURE'
+                    }
+                }
+            }
+        }
+        stage("Publish SQL Script") {
+            steps {
+                script {
+                    try {
+                        if(env.JOB_NAME == "MSR-FSR/Answer2.0/stage") {
+                            bat label: '', script: 'sqlpackage.exe /a:publish /SourceFile:%WORKSPACE%\\Msr.Database\\bin\\Release\\Msr.Database.dacpac /TargetConnectionString:"Data Source=bang.msr-fsr.com;Initial Catalog=Answer2_Stage;User Id=sa;Password=L8xvg2FGqs7CEQ+a;Integrated Security=true"'
+                        } else {
+                            echo "Only publishing a script for Stage"
+                        }
+                    } catch(e) {
+                        office365ConnectorSend color: "${RED}", message: "${JOB_NAME} build FAILED publishing SQL script from Msr.Database. \n Error: ${e}", status: 'Failed', webhookUrl: "${WEBHOOK_URL}"
                     }
                 }
             }
@@ -117,7 +134,7 @@ pipeline {
                         try {
                             if(env.JOB_NAME == "MSR-FSR/Answer2.0/dev") {
                                 deploy("${APP_NAME_DEV}","${DEPLOY_GROUP_DEV}")
-                            } else if(env.JOB_NAME == "MSR-FSR/Answer2.0/Stage") {
+                            } else if(env.JOB_NAME == "MSR-FSR/Answer2.0/stage") {
                                 deploy("${APP_NAME_STAGE}","${DEPLOY_GROUP_STAGE}")
                             } else if(env.JOB_NAME == "MSR-FSR/Answer2.0/master") {
                                 deploy("${APP_NAME_PROD}","${DEPLOY_GROUP_PROD}")
