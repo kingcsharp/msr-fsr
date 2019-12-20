5151be618915809d441c354709ff385d48dc9c97
David Dombrowsky <davek@6thstreetradio.org>
2019-12-20 11:05:22 -0500

    removed more duplicate files

--- Msr.Database/dbo/Views/Sensor_Current_Values.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE VIEW [dbo].[Sensor_Current_Values]
-	AS SELECT        S.Site, S.SensorName, m.ItemCurrentValue
-FROM            dbo.Sensor_Mapping AS S INNER JOIN
-                         EquipmentMonitoring.dbo.Hillsboro_UpdateTable AS m ON m.ItemID = S.MonitoringID
