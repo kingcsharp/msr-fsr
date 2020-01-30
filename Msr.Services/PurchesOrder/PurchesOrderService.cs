b1eb9500d4042d7f2ae6419406df1d9a14d8d0f4
David Dombrowsky <davek@6thstreetradio.org>
2020-01-29 22:36:22 -0500

    reapplied f924c324 2019-05-25 11:57:12 -0300 alecellis1985: allow grouping of po's

--- Msr.Services/PurchesOrder/PurchesOrderService.cs
+++ Msr.Services/PurchesOrder/PurchesOrderService.cs
@@ -150,6 +150,7 @@ namespace Msr.Services.PurchesOrder
             }
 
             var sql = $"SELECT * FROM A_V_FILLS_SEARCH WHERE ID IN(" + str + ") AND FILL_OBJ_ID IS NULL and QTY_NEEDS_FILLING > 0";
+
             var fills = _dbContext.Database.SqlQuery<PurchaseOrderFill>(sql).Select(x => new PurchaseOrderFillViewModel()
             {
                 CUST_NAME = x.CUST_NAME,
@@ -373,7 +374,7 @@ namespace Msr.Services.PurchesOrder
         public ResultNotification<string> PurchasedOrderUpdateAndShowOrderItemList(PurchaseFormAccountViewModel model, string ntLogin)
         {
             var responsePurchase = new ResultNotification<string>();
-            int count = 0;
+            //int count = 0;
             try
             {
 
@@ -393,33 +394,34 @@ namespace Msr.Services.PurchesOrder
 
                 for (int i = 0; i < model.ProductPo.Count; i++)
                 {
-                    count++;
-                    purchasePoViewItemProcedure.purchObjID = purchasePoProcedure.NewID;
-                    if (purchasePoDetailProcedure.NewID == null)
+                    //count++;
+                    var poCreations = model.ProductPo[i].GroupPo ? 1 : model.ProductPo[i].Qty;
+                    for (var j = 0; j < poCreations; j++)
                     {
-                        purchasePoViewItemProcedure.orderItemID = null;
-                    }
-                    else
-                    {
-                        purchasePoViewItemProcedure.orderItemID = purchasePoDetailProcedure.NewID;
-                    }
-                    purchasePoViewItemProcedure.qty = model.ProductPo[i].Qty.ToString();
-                    purchasePoViewItemProcedure.acctID = model.Root;
-                    purchasePoViewItemProcedure.strNTLogin = ntLogin;
-                    purchasePoViewItemProcedure.GroupWO = model.ProductPo[i].GroupWO;
+                        purchasePoViewItemProcedure.purchObjID = purchasePoProcedure.NewID;
 
-                    _dbContext.Database.ExecuteStoredProcedure(purchasePoViewItemProcedure);
+                        if (purchasePoDetailProcedure.NewID == null)
+                        {
+                            purchasePoViewItemProcedure.orderItemID = null;
+                        }
+                        else
+                        {
+                            purchasePoViewItemProcedure.orderItemID = purchasePoDetailProcedure.NewID;
+                        }
+                        purchasePoViewItemProcedure.qty = model.ProductPo[i].GroupPo ? model.ProductPo[i].Qty.ToString() : "1";
+                        purchasePoViewItemProcedure.acctID = model.Root;
+                        purchasePoViewItemProcedure.strNTLogin = ntLogin;
+                        _dbContext.Database.ExecuteStoredProcedure(purchasePoViewItemProcedure);
 
 
-                    if (count != model.ProductPo.Count)
-                    {
+                        //if (i != model.ProductPo.Count - 1 || j != poCreations - 1)
+                        //{
                         purchasePoDetailProcedure.strPurchaseObjID = purchasePoProcedure.NewID;
-                        purchasePoDetailProcedure.orderID = model.ProductPo[i + 1].ORDER_ID;
+                        purchasePoDetailProcedure.orderID = model.ProductPo[i].ORDER_ID;
                         purchasePoDetailProcedure.Strntlogin = ntLogin;
                         _dbContext.Database.ExecuteStoredProcedure(purchasePoDetailProcedure);
-
+                        //}
                     }
-
                 }
 
                 _dbContext.Database.ExecuteSqlCommand("UPDATE A_PURCHASES_HISTORY SET  CUST_PURCH_NUM = '" + model.REFERENCE_PO + "', ACCT_FOR_ALL = '" + model.Root + "' WHERE OBJECT_ID = '" + responsePurchase.Entity + "'");
@@ -440,78 +442,6 @@ namespace Msr.Services.PurchesOrder
             }
             return responsePurchase;
         }
-
-        //public ResultNotification<string> PurchasedOrderUpdateAndShowOrderItemList(PurchaseFormAccountViewModel model, string ntLogin)
-        //{
-        //    var responsePurchase = new ResultNotification<string>();
-        //    //int count = 0;
-        //    try
-        //    {
-
-        //        var orderid = model.ProductPo.Select(x => x.ORDER_ID).FirstOrDefault();
-
-        //        var purchasePoProcedure = new PurchasePoProcedure();
-        //        purchasePoProcedure.Orderid = orderid;
-        //        purchasePoProcedure.Strntlogin = ntLogin;
-
-        //        _dbContext.Database.ExecuteStoredProcedure(purchasePoProcedure);
-        //        responsePurchase.Entity = purchasePoProcedure.NewID;
-
-        //        _dbContext.Database.ExecuteSqlCommand("UPDATE A_PURCHASES_HISTORY SET  CUST_PURCH_NUM = '" + model.REFERENCE_PO + "', ACCT_FOR_ALL = '" + model.Root + "' WHERE OBJECT_ID = '" + responsePurchase.Entity + "'");
-
-        //        var purchasePoViewItemProcedure = new PurchasePoViewItemProcedure();
-        //        var purchasePoDetailProcedure = new PurchasePoDetailProcedure();
-
-        //        for (int i = 0; i < model.ProductPo.Count; i++)
-        //        {
-
-        //            //count++;
-        //            var poCreations = model.ProductPo[i].GroupPo ? 1 : model.ProductPo[i].Qty;
-        //            for (var j = 0; j < poCreations; j++)
-        //            {
-        //                purchasePoViewItemProcedure.purchObjID = purchasePoProcedure.NewID;
-
-        //                if (purchasePoDetailProcedure.NewID == null)
-        //                {
-        //                    purchasePoViewItemProcedure.orderItemID = null;
-        //                }
-        //                else
-        //                {
-        //                    purchasePoViewItemProcedure.orderItemID = purchasePoDetailProcedure.NewID;
-        //                }
-        //                purchasePoViewItemProcedure.qty = model.ProductPo[i].GroupPo ? model.ProductPo[i].Qty.ToString() : "1";
-        //                purchasePoViewItemProcedure.acctID = model.Root;
-        //                purchasePoViewItemProcedure.strNTLogin = ntLogin;
-        //                _dbContext.Database.ExecuteStoredProcedure(purchasePoViewItemProcedure);
-
-
-        //                //if (i != model.ProductPo.Count - 1 || j != poCreations - 1)
-        //                //{
-        //                purchasePoDetailProcedure.strPurchaseObjID = purchasePoProcedure.NewID;
-        //                purchasePoDetailProcedure.orderID = model.ProductPo[i].ORDER_ID;
-        //                purchasePoDetailProcedure.Strntlogin = ntLogin;
-        //                _dbContext.Database.ExecuteStoredProcedure(purchasePoDetailProcedure);
-        //            }
-        //        }
-
-        //        _dbContext.Database.ExecuteSqlCommand("UPDATE A_PURCHASES_HISTORY SET  CUST_PURCH_NUM = '" + model.REFERENCE_PO + "', ACCT_FOR_ALL = '" + model.Root + "' WHERE OBJECT_ID = '" + responsePurchase.Entity + "'");
-
-        //        var purchase = PurchasedOrderById(responsePurchase.Entity);
-
-        //        var id = new SqlParameter("@pHistID", purchase.ID);
-        //        var ntlog = new SqlParameter("@strNTLogin", ntLogin);
-        //        _dbContext.Database.ExecuteSqlCommand("exec A_SP_PURCHASES_UPDATE_ALL_ACCOUNTS_ON_PURCHASE_ITEMS @pHistID,@strNTLogin", id, ntlog);
-
-        //    }
-        //    catch (Exception ex)
-        //    {
-
-        //        responsePurchase.AddError(ex.Message);
-
-        //        return responsePurchase;
-        //    }
-        //    return responsePurchase;
-        //}
 
         public ResultNotification<AddPurchaseResponse> Create(NewPurchaseOrderViewModel model)
         {
