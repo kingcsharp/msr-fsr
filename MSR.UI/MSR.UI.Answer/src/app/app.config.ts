ec0246dc041636629a230ff446b3674d60a0d2aa
Tim Millett <aydensoft@gmail.com>
2020-05-14 00:02:36 -0400

    Fix deployment

--- MSR.UI/MSR.UI.Answer/src/app/app.config.ts
+++ MSR.UI/MSR.UI.Answer/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 
-const hostApi = environment.production ? 'http://dev-api.answer.msr-fsr.com' : 'http://localhost';
+const hostApi = environment.production ? 'https://dev-api.answer.msr-fsr.com' : 'http://localhost';
 const portApi = environment.production ? 80 : 17777;
 const baseURLApi = `${hostApi}${portApi ? `:${portApi}` : ``}/${environment.version}`;
 
