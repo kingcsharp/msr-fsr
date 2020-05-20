b68568092c64692a4b1fdbd4d78c0d60d3e23e0b
alecellis1985 <alecellis1985@gmail.com>
2020-05-20 13:27:58 -0300

    start moving the get icon to globals.

--- MSR.UI/MSR.UI.Answer/src/app/models/lib/globals.ts
+++ MSR.UI/MSR.UI.Answer/src/app/models/lib/globals.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, NavigationError, NavigationCancel, RoutesRecognized } from '@angular/router';
 
 @Injectable()
 export class Globals {
@@ -8,7 +9,21 @@ export class Globals {
     loader = true;
     user;
 
-    constructor() {
+    constructor(private router: Router) {
+        router.events.forEach((event) => {
+            if (event instanceof NavigationEnd) {
+                const userInfo = JSON.parse(localStorage.getItem('user'));
+                debugger;
+                let splitUrl = event.url.split('/');
+
+                const menu = userInfo.roles[0].menus.filter(x => x.url.toLowerCase() === splitUrl[splitUrl.length-1]);
+                
+
+                // this.sidebarItems = this.generateMenu(userInfo.roles[0].menus);
+            }
+
+        });
+
         if (localStorage.user === undefined || localStorage.user === undefined) {
             delete localStorage.user;
             delete localStorage.token;
@@ -22,6 +37,26 @@ export class Globals {
         }
     }
 
+    generateMenu(menuItems: any) {
+        let menuStructure: any = [];
+        //show tooltip add .
+        //description
+        menuItems.forEach(function (item) {
+            const elem = menuStructure.find(x => x.name === item.menuGroup.name);
+            if (elem === undefined) {
+                let menuItem = { submenu: [{ name: item.name, url: item.url, icon: item.icon, orderNr: item.orderNumber, info: item.info }] };
+                Object.assign(menuItem, item.menuGroup);
+                menuStructure.push(menuItem);
+            } else {
+                const submenuItem = elem.submenu.find(x => x.name === item.name);
+                if (submenuItem === undefined) {
+                    elem.submenu.push({ name: item.name, url: item.url, icon: item.icon, orderNr: item.orderNumber, info: item.info });
+                }
+            }
+        });
+        return menuStructure;
+    }
+
     showLoader(isOn) {
         this.loader = isOn;
     }
@@ -32,11 +67,11 @@ export class Globals {
         return ret;
     }
 
-    getGridTitle(){
+    getGridTitle() {
 
     }
-    setGridTitle(){
-        
+    setGridTitle() {
+
     }
 
     updateLogin(val) {
