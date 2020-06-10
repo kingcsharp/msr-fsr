8ba951deb1f2f055477d4f7616c1b08052c1d156
Robert Lara III <admin@robertlara.me>
2020-06-10 01:16:25 -0500

    Added Forgot Password Test Case

--- MSR.UI/MSR.UI.Answer/cypress/integration/user_functionality/TC365.2_UsersCanResetPassword.js
+++ MSR.UI/MSR.UI.Answer/cypress/integration/user_functionality/TC365.2_UsersCanResetPassword.js
@@ -1,13 +1,21 @@
 describe('User Functionality', () => {
     it('TC365.2_UsersCanResetPassword', () => {
 
+        cy.server()
+
+        cy.route({
+            method: 'POST',      // Route all GET requests
+            url: '/v1/Account/forgotpassword',    // that have a URL that matches '/users/*'
+            response: []        // and force the response to be: []
+          })
+
         cy.visit('/')
 
         cy.url().should('include', '/login')
 
         cy.get('[data-cy=forgotpassword-link]').click()
 
-        cy.get('[data-cy=username-input]').type('rlara').should('have.value','rlara')
+        cy.get('[data-cy=username-input]').type('admin').should('have.value','admin')
 
         cy.get('[data-cy=submit-button]').click()
 
