2bb0a8e5c91906fd3fb41811fa4986452b4ed12d
David Dombrowsky <davek@6thstreetradio.org>
2020-06-30 07:50:28 -0700

    fixed response for DELETE WorkflowStage

--- MSR.Answer.API/V1/Controllers/Workflow/WorkflowStageController.cs
+++ MSR.Answer.API/V1/Controllers/Workflow/WorkflowStageController.cs
@@ -59,7 +59,8 @@ namespace MSR.Answer.API.V1.Controllers.Workflow
             return ret.ToOkObjectResponse<WorkflowStageModel>("Workflow Stage has been successfully updated.");
         }
 
-        [HttpDelete("{workflowId}"), SwaggerResponse(typeof(void))]
+        [HttpDelete("{workflowId}")]
+        [ProducesResponseType(typeof(void), 204)]
         public async Task<IActionResult> Delete(int workflowId)
         {
             var ret = await _dispatcher.DispatchAsync(new DeactivateWorkflow()
