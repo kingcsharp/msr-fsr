94c9217d0bf9964ac866430883312e58ed6bd362
Robert Lara <admin@robertlara.me>
2020-07-18 13:30:07 -0500

    Added Grid to help page, still need roles filter, action buttons, and to move add button

--- MSR.UI/MSR.UI.Answer/src/app/pages/help/help-create/help-create.component.ts
+++ MSR.UI/MSR.UI.Answer/src/app/pages/help/help-create/help-create.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { HelpService, CreateHelpPageRequest, RoleService, Role } from '../../../services/api.client.generated';
 import { environment as env } from '../../../../environments/environment';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { take } from 'rxjs/operators';
+import { responseHandler } from '../../../utils/responseHandler';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-help-create',
@@ -13,7 +17,9 @@ export class HelpCreateComponent implements OnInit {
   createHelpPageRequest: CreateHelpPageRequest = new CreateHelpPageRequest();
   availableRoles: Role[] = new Array<Role>();
   selectedRoles: Role[] = new Array<Role>();
-  constructor(private helpService:HelpService, private roleService:RoleService) { }
+  public Editor = ClassicEditor;
+
+  constructor(private helpService:HelpService, private roleService:RoleService, private location: Location) { }
 
   ngOnInit(): void {
 
@@ -25,6 +31,18 @@ export class HelpCreateComponent implements OnInit {
 
   saveNewHelpPage(){
     this.createHelpPageRequest.roleIds = this.selectedRoles.filter((selectedRole) => this.availableRoles.find(role => role.name === selectedRole.name)).map(s => s.id);
+    
+    /* TODO fix when you merge with Alec's Workflow branch to fix the bug coming up here
+    this.helpService.helpPost(env.apiVersion,this.createHelpPageRequest).subscribe(responseHandler((resp) => {
+      if (!resp.hasErrors) {
+        console.log(resp);
+      }
+    }, (error) => {
+      console.log(error);
+    }));
+    */
+
+   this.location.go('help/help');
   }
 
 }
