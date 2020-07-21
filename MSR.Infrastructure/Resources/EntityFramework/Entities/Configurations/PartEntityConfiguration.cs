b0a2bd48c3953a83c425f57a9943c3861f723fbf
David Dombrowsky <davek@6thstreetradio.org>
2020-07-20 20:09:12 -0700

    added EF configuration for PartSubPartMap

---
+++ MSR.Infrastructure/Resources/EntityFramework/Entities/Configurations/PartEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MSR.Infrastructure.Resources.EntityFramework.Entities.Configurations
+{
+    public class PartEntityConfiguration : IEntityTypeConfiguration<Part>
+    {
+        public void Configure(EntityTypeBuilder<Part> builder)
+        {
+            builder.ToTable(nameof(Part));
+            builder.HasMany(part => part.Subparts).WithOne().HasForeignKey(sp => sp.ParentPartId);
+        }
+    }
+}
---
+++ MSR.Infrastructure/Resources/EntityFramework/Entities/Configurations/PartSubPartMapEntityConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MSR.Infrastructure.Resources.EntityFramework.Entities.Configurations
+{
+    public class PartSubPartMapEntityConfiguration : IEntityTypeConfiguration<PartSubPartMap>
+    {
+        public void Configure(EntityTypeBuilder<PartSubPartMap> builder)
+        {
+            builder.ToTable(nameof(PartSubPartMap));
+            builder.HasOne(sp => sp.ParentPart).WithMany(p => p.Subparts);
+            builder.HasOne(sp => sp.Part);
+        }
+    }
+}
