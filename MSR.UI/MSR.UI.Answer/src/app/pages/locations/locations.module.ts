c1c28cf81a75588201aeed56e17c5933729b1907
Robert Lara III <robertlaraiii@gmail.com>
2020-07-21 12:09:19 -0500

    Added locations page and removed extra spec file

--- MSR.UI/MSR.UI.Answer/src/app/pages/locations/locations.module.ts
+++ MSR.UI/MSR.UI.Answer/src/app/pages/locations/locations.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LocationsComponent } from './locations/locations.component';
 import { RouterModule } from '@angular/router';
+import { TableModule } from 'primeng/table';
+import { NewWidgetModule } from '../../layout/new-widget/widget.module';
+import { GridOptionsComponent } from '../../components/grid-options/grid-options.component';
+import { DialogModule } from 'primeng/dialog';
+import { MultiSelectModule } from 'primeng/multiselect';
 
 export const routes = [
   { path: '', redirectTo: 'locations', pathMatch: 'full' },
@@ -10,10 +15,14 @@ export const routes = [
 
 
 @NgModule({
-  declarations: [LocationsComponent],
+  declarations: [LocationsComponent, GridOptionsComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    TableModule,
+    NewWidgetModule,
+    DialogModule,
+    MultiSelectModule
   ]
 })
 export class LocationsModule {
