5846cee0b7464022467c1c9ae54b111725bcd1da
james <james.bridgeford@cmhworks.com>
2020-07-22 12:02:39 -0500

    adding missing reference

--- Msr.Infrastructure/Extensions/InfrastructureServiceExtensions.cs
+++ Msr.Infrastructure/Extensions/InfrastructureServiceExtensions.cs
@@ -22,6 +22,7 @@ using MSR.Infrastructure.Resources.Services.Help;
 using Amazon.S3;
 using MSR.Domain.Abstractions;
 using MSR.Infrastructure.Factories;
+using MSR.Infrastructure.Resources.AWS;
 
 namespace MSR.Infrastructure.Extensions
 {
@@ -55,6 +56,7 @@ namespace MSR.Infrastructure.Extensions
             services.AddScoped<IAmazonS3>(i => new AmazonS3Client(Amazon.RegionEndpoint.USEast1));
             services.AddSingleton<IFileHandlerFactory, FileHandlerFactory>();
             services.AddScoped<IFileService, FileService>();
+            services.AddScoped<S3FileHandler>();
 
             services.AddScoped<IAuthenticationHelper, AuthenticationHelper>();
 
