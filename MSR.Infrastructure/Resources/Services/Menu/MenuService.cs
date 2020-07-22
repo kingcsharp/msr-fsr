9937db82b66a936e46d09e6fa32997d472f24423
james <james.bridgeford@cmhworks.com>
2020-07-21 23:18:42 -0500

    forgot comma

--- MSR.Infrastructure/Resources/Services/Menu/MenuService.cs
+++ MSR.Infrastructure/Resources/Services/Menu/MenuService.cs
@@ -33,7 +33,7 @@ namespace MSR.Infrastructure.Resources.Services.Menu
             {
                 var domainMenuItem = new Domain.Models.MenuItem()
                 {
-                    Id = efMenuItem.Id
+                    Id = efMenuItem.Id,
                     Icon = efMenuItem.Icon,
                     Info = efMenuItem.Info,
                     Name = efMenuItem.Name,
