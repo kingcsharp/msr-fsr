c3c27766cb9c935e4912e9804fc71481cefa4be0
Robert Lara III <robertlaraiii@gmail.com>
2020-07-22 21:19:09 -0500

    Added LookUpItems for Country

--- MSR.UI/MSR.UI.Answer/src/app/pages/main/customer-create/customer-create.component.ts
+++ MSR.UI/MSR.UI.Answer/src/app/pages/main/customer-create/customer-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CustomerService } from '../../../services/api.client.generated';
+import { CustomerService, Customer, CreateCustomerRequest, ICustomer } from '../../../services/api.client.generated';
 import { ActivatedRoute } from '@angular/router';
 import { environment as env } from '../../../../environments/environment';
 import { responseHandler } from '../../../utils/responseHandler';
@@ -7,25 +7,32 @@ import { responseHandler } from '../../../utils/responseHandler';
 @Component({
   selector: 'app-customer-create',
   templateUrl: './customer-create.component.html',
-  styleUrls: ['./customer-create.component.scss']
+  styleUrls: ['./customer-create.component.scss'],
+  providers: [CustomerService]
 })
 export class CustomerCreateComponent implements OnInit {
 
-  customer:any;
+  customer:Customer = null;
+  customerToEditId:number = 0;
   constructor(private customerService: CustomerService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
 
     this.route.queryParams.subscribe(params => {
-      this.customer.id = params['id'] == null ? 0 : params['id'];
+      this.customerToEditId = params['id'] == null ? 0 : params['id'];
 
-      if (this.customer.id !== 0) {
+      if (this.customerToEditId !== 0) {
 
-          this.customerService.customerGet(this.customer.id,null,null,null,null,null,null,null,env.apiVersion).subscribe(responseHandler((response) => {
+          this.customerService.customerGet(this.customerToEditId,null,null,null,null,null,null,null,env.apiVersion).subscribe(responseHandler((response) => {
             
             console.log(response);
 
           }));
+
+      }else{
+
+        this.customer = new Customer();
+        console.log(this.customer);
 
       }
 
