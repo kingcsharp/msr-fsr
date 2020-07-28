50fd91a03b927f740d0980061181f6b3308e3f04
David Dombrowsky <davek@6thstreetradio.org>
2020-07-27 18:51:26 -0700

    added skeleton files for WorkOrder

---
+++ MSR.Answer.API/V1/Models/UpdateWorkOrderRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MSR.Answer.API.V1.Models
+{
+    public class UpdateProcedureTypeRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ MSR.Domain/Abstractions/Services/IWorkOrderService.cs
@@ -0,0 +1,14 @@
+﻿using MSR.Domain.Commands;
+using MSR.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MSR.Domain.Abstractions.Services
+{
+    public interface IProcedureTypeService
+    {
+        Task<ICollection<ProcedureType>> GetProcedureTypeAsync(GetProcedureType command);
+        Task<ProcedureType> CreateProcedureTypeAsync(CreateProcedureType command);
+        Task<ProcedureType> UpdateProcedureTypeAsync(UpdateProcedureType command);
+    }
+}
