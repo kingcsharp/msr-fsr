34840dc3c68f5c0af01c7f549c74619aa1b7be9b
David Dombrowsky <davek@6thstreetradio.org>
2020-08-10 14:54:34 -0700

    added MockQueryable and mocks for a bunch of tables

--- MSR.Infrastructure.Tests/ClassFixtures/Resources/Services/AccountServiceTestSetup.cs
+++ MSR.Infrastructure.Tests/ClassFixtures/Resources/Services/AccountServiceTestSetup.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using MSR.Infrastructure.Resources.Services.Account;
+using MSR.Infrastructure.Resources.Services.Workflow;
 using Moq;
 using MSR.Domain.Abstractions.Services;
+using MSR.Domain.Abstractions.Services.Workflow;
 using MSR.Infrastructure.Resources.EntityFramework.Entities;
 using MSR.Infrastructure.Resources.EntityFramework.Application;
 using MSR.Infrastructure.Resources.EntityFramework.Interfaces;
@@ -17,6 +19,7 @@ using System.Threading.Tasks;
 using MSR.Domain.Models.Config;
 using System.Linq;
 using MSR.Infrastructure.Helpers.Abstractions;
+using MockQueryable.Moq;
 
 namespace MSR.Infrastructure.Tests.ClassFixtures.Resources.Services
 {
@@ -31,6 +34,54 @@ namespace MSR.Infrastructure.Tests.ClassFixtures.Resources.Services
             var mockEmailService = new Mock<IEmailService>();
             var mockAuthenticationHelper = new Mock<IAuthenticationHelper>();
 
+            var workflowGroupUserMap = new Mock<IRepository<WorkflowGroupUserMap>>();
+            var workflowGroupUserMock = new List<WorkflowGroupUserMap>().AsQueryable().BuildMock();
+            workflowGroupUserMap.Setup(m => m.Query()).Returns(workflowGroupUserMock.Object);
+            mockUnitOfWork.SetupGet(m => m.WorkflowGroupUserMaps)
+                .Returns(workflowGroupUserMap.Object);
+
+            var workflowGroupStageMap = new Mock<IRepository<WorkflowGroupStageMap>>();
+            var workflowGroupStageMapMock = new List<WorkflowGroupStageMap>().AsQueryable().BuildMock();
+            workflowGroupStageMap.Setup(m => m.Query()).Returns(workflowGroupStageMapMock.Object);
+            mockUnitOfWork.SetupGet(m => m.WorkflowGroupStageMaps)
+                .Returns(workflowGroupStageMap.Object);
+
+            var workflowActivityMap = new Mock<IRepository<WorkflowActivityMap>>();
+            var workflowActivityMapMock = new List<WorkflowActivityMap>().AsQueryable().BuildMock();
+            workflowActivityMap.Setup(m => m.Query()).Returns(workflowActivityMapMock.Object);
+            mockUnitOfWork.SetupGet(m => m.WorkflowActivityMaps)
+                .Returns(workflowActivityMap.Object);
+
+            var workflowGroups = new Mock<IRepository<WorkflowGroup>>();
+            var workflowGroupsMock = new List<WorkflowGroup>().AsQueryable().BuildMock();
+            workflowGroups.Setup(m => m.Query()).Returns(workflowGroupsMock.Object);
+            mockUnitOfWork.SetupGet(m => m.WorkflowGroups)
+                .Returns(workflowGroups.Object);
+
+            var workflowStages = new Mock<IRepository<WorkflowStage>>();
+            var workflowStagesMock = new List<WorkflowStage>().AsQueryable().BuildMock();
+            workflowStages.Setup(m => m.Query()).Returns(workflowStagesMock.Object);
+            mockUnitOfWork.SetupGet(m => m.WorkflowStages)
+                .Returns(workflowStages.Object);
+
+            var workflowStagesMap = new Mock<IRepository<WorkflowStageMap>>();
+            var workflowStagesMapMock = new List<WorkflowStageMap>().AsQueryable().BuildMock();
+            workflowStagesMap.Setup(m => m.Query()).Returns(workflowStagesMapMock.Object);
+            mockUnitOfWork.SetupGet(m => m.WorkflowStageMaps)
+                .Returns(workflowStagesMap.Object);
+
+            var workflowActivities = new Mock<IRepository<WorkflowActivity>>();
+            var workflowActivitiesMock = new List<WorkflowActivity>().AsQueryable().BuildMock();
+            workflowActivities.Setup(m => m.Query()).Returns(workflowActivitiesMock.Object);
+            mockUnitOfWork.SetupGet(m => m.WorkflowActivities)
+                .Returns(workflowActivities.Object);
+
+            var menuRoles = new Mock<IRepository<MenuRole>>();
+            var menuRolesMock = new List<MenuRole>().AsQueryable().BuildMock();
+            menuRoles.Setup(m => m.Query()).Returns(menuRolesMock.Object);
+            mockUnitOfWork.SetupGet(m => m.MenuRoles)
+                .Returns(menuRoles.Object);
+
             Expression<Func<User, bool>> testExpression = i => i.UserName == Constants.GoodUserName;
 
             mockIRepositoryUser.Setup(m => m.FirstOrDefault(It.IsAny<bool>(),
@@ -40,10 +91,10 @@ namespace MSR.Infrastructure.Tests.ClassFixtures.Resources.Services
             mockIRepositoryUser.Setup(m => m.FirstOrDefaultAsync(It.IsAny<bool>(),
                                         It.IsAny<Expression<Func<User, bool>>>(), null))
                 .ReturnsAsync(EFUserTestFixture.GoodUser);
-            
-            mockIRepositoryUser.Setup(m => m.Query())
-                .Returns(new List<User>() { EFUserTestFixture.GoodUser }.AsQueryable());
-                       
+
+            var usersMock = new List<User>() { EFUserTestFixture.GoodUser }.AsQueryable().BuildMock();
+            mockIRepositoryUser.Setup(m => m.Query()).Returns(usersMock.Object);
+
             mockUnitOfWork.SetupGet(m => m.Users)
                 .Returns(mockIRepositoryUser.Object);
 
@@ -61,22 +112,23 @@ namespace MSR.Infrastructure.Tests.ClassFixtures.Resources.Services
             var services = new ServiceCollection()
             .AddLogging()
             .AddScoped<IAccountService, AccountService>()
+            .AddScoped<IWorkflowService, WorkflowService>()
             .AddAutoMapper(typeof(InfrastructureMappingProfiles))
             .AddSingleton(mockIRepositoryUser.Object)
             .AddSingleton(mockUnitOfWork.Object)
             .AddSingleton(mockEmailService.Object)
             .AddSingleton(mockAuthenticationHelper.Object)
             .AddSingleton(new JwtData() { Secret = "ks1YvoEvXrofzYLABCDE9hNqM9gafA0cciHg7S31nwATocYvhjj2VF5xTAd5z1NQQid6zqAEUcaDBpuXeRP8A3hbURV10NEESXxSlPWjLINn4U6DEAysjPWeexsXPI" })
-            .AddSingleton(new EmailInformation() 
-            { 
-                EnableSsl = false, 
-                From = "test@test.com", 
-                Host = "test.com", 
-                Password = "test", 
-                PasswordReminderEmailsTo = "test@test.com", 
-                Port = 443, SendEmailsTo = "test@test.com", 
-                SupportEmail = "test@test.com", 
-                UserName = "Test" 
+            .AddSingleton(new EmailInformation()
+            {
+                EnableSsl = false,
+                From = "test@test.com",
+                Host = "test.com",
+                Password = "test",
+                PasswordReminderEmailsTo = "test@test.com",
+                Port = 443, SendEmailsTo = "test@test.com",
+                SupportEmail = "test@test.com",
+                UserName = "Test"
             })
             .AddSingleton(new GeneralInformation()
             {
@@ -84,7 +136,7 @@ namespace MSR.Infrastructure.Tests.ClassFixtures.Resources.Services
                 RollbarConfig = "test",
                 WebsiteURL = "test.com"
             });
-            
+
             ServiceProvider = services.BuildServiceProvider();
         }
     }
