223bc90c8b74beb61f0538a615cac00a119961bb
starsoftdev <derek.otieno.2020@gmail.com>
2020-08-12 05:09:12 -0400

    add custom switch button

---
+++ MSR.UI/MSR.UI.Answer/src/app/components/bootstrap-switch/bootstrap-switch.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input, Output, EventEmitter, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'bootstrap-switch',
+  templateUrl: './bootstrap-switch.component.html',
+  styleUrls: ['./bootstrap-switch.component.scss']
+})
+export class BootstrapSwitchComponent implements OnInit {
+
+  @Input() isOn: boolean = false;
+  @Output() onChange = new EventEmitter<boolean>();
+
+  toggle: boolean;
+  marginLeft: number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.marginLeft =  this.isOn ?  0 : -53;
+  }
+
+  onClickToggle() {
+    console.log(this.isOn)
+    this.onChange.emit(!this.isOn);
+  }
+
+}
