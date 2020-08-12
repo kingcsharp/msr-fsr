909ace768fd4bf5c8c8840308270c39d4086f533
starsoftdev <derek.otieno.2020@gmail.com>
2020-08-12 05:14:48 -0400

    Update product definition page

--- MSR.UI/MSR.UI.Answer/src/app/pages/pricing/product-definition/product-definition.component.ts
+++ MSR.UI/MSR.UI.Answer/src/app/pages/pricing/product-definition/product-definition.component.ts
@@ -1,15 +1,167 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  PartService, PartModel, SubPartModel, EnumApprovalTables, AuditActionResultOfPartModel, CreatePartRequest, UpdatePartRequest, FileModel,
+  ProcedureService, Procedure,
+  ProcedureStepTemplateService, ProcedureStepTemplate,
+  LocationService, LocationModel,
+  CustomerService,
+  ProcedureStep
+} from '../../../services/api.client.generated';
+import { take } from 'rxjs/operators';
+import { environment as env } from '../../../../environments/environment';
+import { EnumPrivilege, EnumMenuItem } from '../../../models/enums/privileges';
+import { responseHandler } from '../../../utils/responseHandler';
+import { ViewSaved } from '../../../models/lib/ViewSaved';
+import { ColumnsSaved } from '../../../models/lib/ColumnsSaved';
+import { CommonGrid } from '../../../models/lib/CommonGrid';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { Globals } from '../../../models/lib/globals';
 
 @Component({
   selector: 'app-product-definition',
   templateUrl: './product-definition.component.html',
-  styleUrls: ['./product-definition.component.scss']
+  styleUrls: ['./product-definition.component.scss'],
+  providers: [CustomerService, ProcedureService]
 })
 export class ProductDefinitionComponent implements OnInit {
+  showAllStepColumns: boolean = false;
+  partsData: any[] = [];
+  getPartsFlag: boolean = false;
+  proceduresData: any[] = [];
+  getProceduresFlag: boolean = false;
+  locationsData: any[] = [];
+  getLocationsFlag: boolean = false;
+  customersData: any[] = [];
+  getCustomersFlag: boolean = false;
+  productData: ProductModel;
+  isRefreshingPartsData: boolean = false;
+  isRefreshingProceduresData: boolean = false;
+  getProcedureStepsFlag: boolean = true;
 
-  constructor() { }
+  constructor(
+    public globals: Globals,
+    private partsService: PartService,
+    private locationService: LocationService,
+    private customerService: CustomerService,
+    private procedureService: ProcedureService,
+  ) { }
 
   ngOnInit(): void {
+    this.productData = new ProductModel;
+    this.getLocations();
+    this.getCustomers();
+    this.getParts();
+    this.getProcedures();
+  }
+
+  getCustomers() {
+    const ctrl = this;
+    if (ctrl.getCustomersFlag) {
+      return ctrl.customersData;
+    }
+    this.customerService.customerGet(null, null, null, null, null, null, null, null, env.apiVersion).subscribe(responseHandler((response) => {
+      response.object.map((x) => {
+        ctrl.customersData.push({ label: `[MSR-FSR] ${x.name} - [ID: ${x.id}]`, value: x.id });
+      });
+      ctrl.getCustomersFlag = true;
+    }));
+  }
+
+  getLocations() {
+    const ctrl = this;
+    if (ctrl.getLocationsFlag) {
+      return ctrl.locationsData;
+    }
+    this.globals.showLoader(true);
+    this.locationService.locationGet(null, env.apiVersion).subscribe(responseHandler((response) => {
+      response.object.map((x) => {
+        ctrl.locationsData.push({ label: x.name, value: x.id });
+      });
+      ctrl.getLocationsFlag = true;
+    }));
+  }
+
+  getParts(isRefresh: boolean = false) {
+    const ctrl = this;
+
+    if (isRefresh) {
+      ctrl.isRefreshingPartsData = true;
+      ctrl.partsData = [];
+    }
+
+    if (ctrl.getPartsFlag && !isRefresh) {
+      return ctrl.partsData;
+    }
+
+    this.partsService.partGet(null, env.apiVersion).pipe(take(1))
+      .subscribe(responseHandler(response => {
+        response.object.map((x) => {
+          ctrl.partsData.push({ label: `${x.name} [${x.partNumber}] [ID: ${x.id}]`, value: x.id });
+        });
+        ctrl.getPartsFlag = true;
+        if (isRefresh) {
+          ctrl.isRefreshingPartsData = false;
+        }
+      }));
+  }
+
+  getProcedures(isRefresh: boolean = false) {
+    const ctrl = this;
+
+    if (isRefresh) {
+      ctrl.isRefreshingProceduresData = true;
+      ctrl.proceduresData = [];
+    }
+    if (ctrl.getProceduresFlag && !isRefresh) {
+      return ctrl.proceduresData;
+    }
+    this.procedureService.procedureGet(null, env.apiVersion).pipe(take(1))
+      .subscribe(responseHandler(response => {
+        response.object.map((x) => {
+          ctrl.proceduresData.push({ label: `${x.name} [ID: ${x.id}]`, value: x.id });
+        });
+        ctrl.getProceduresFlag = true;
+        if (isRefresh) {
+          ctrl.isRefreshingProceduresData = false;
+        }
+      }));
+  }
+
+  getProcedureSteps(id: number) {
+    const ctrl = this;
+
+    ctrl.getProcedureStepsFlag = false;
+    ctrl.productData.procedureSteps = [];
+    this.procedureService.stepGet(id, null, env.apiVersion).pipe(take(1))
+    .subscribe(responseHandler(response => {
+      ctrl.productData.procedureSteps = response.object;
+      ctrl.getProcedureStepsFlag = true;
+    }));
+  }
+
+  onSelectProcedure($event) {
+    this.getProcedureSteps($event.target.value)
+  }
+
+  onToggle($event: boolean) {
+    this.showAllStepColumns = $event;
+  }
+
+
+  onSubmit() {
+    // TODO: Submit Functionality
+    console.log('_______', this.productData)
   }
 
 }
+
+class ProductModel {
+  locationId: number;
+  customerId: number;
+  partId: number;
+  procedureId: number;
+  procedureSteps: Procedure[];
+}
+
+
