51324fa95928e5e70ec347ed9be79194e8e38d7e
David Dombrowsky <davek@6thstreetradio.org>
2020-08-12 13:03:57 -0700

    implemented work order test fixture

---
+++ MSR.Infrastructure.Tests/TestFixtures/WorkOrderFixture.cs
@@ -0,0 +1,20 @@
+﻿
+using MSR.Domain.Commands;
+using MSR.Infrastructure.Resources.EntityFramework.Entities;
+
+namespace MSR.Infrastructure.Tests.TestFixtures
+{
+    public static class WorkOrderFixture
+    {
+        public static GetWorkOrder AllWorkOrders => new GetWorkOrder(){ Id = null };
+        public static GetWorkOrder NotFoundWorkOrder => new GetWorkOrder(){ Id = 31337 };
+        public static UpdateWorkOrder WorkOrderUpdate => new UpdateWorkOrder(){ Id = 1, Price = 234.56M };
+        public static CreateWorkOrder WorkOrderCreate => new CreateWorkOrder(){ Price = 456.78M };
+        public static DeleteWorkOrder WorkOrderDelete => new DeleteWorkOrder(){ Id = 1 };
+        public static WorkOrder PlainWorkOrder => new WorkOrder()
+        {
+            Id = 1,
+            Price = 123.45M
+        };
+    }
+}
