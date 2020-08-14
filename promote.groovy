7bc6a345d63acd808740442ec9454471f500ca99
Tim Millett <>
2020-08-14 02:41:17 -0400

    Update container tags

--- promote.groovy
+++ promote.groovy
@@ -38,12 +38,17 @@ pipeline {
                 }
             }
         }
-        stage("Promoting to Stage") {
+        stage("Promoting UI to Stage") {
             steps {
                 script {
                     deploy("${UI_COMPOSE}", "${STAGE_PROJECT_UI}", "${STAGE_UI_TARGET_ARN}", "app")
+                }
+            }
+        }
+        stage("Promoting API to Stage") {
+            steps {
+                script {
                     deploy("${API_COMPOSE}", "${STAGE_PROJECT_API}", "${STAGE_API_TARGET_ARN}", "reverseproxy")
-
                 }
             }
         }
