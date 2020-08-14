3785f70353d77b114692118eadee54f3ee112f29
james <james.bridgeford@cmhworks.com>
2020-08-13 23:48:02 -0500

    Fixes for Approval items, and SQSConsumerService

--- MSR.Infrastructure/Resources/EntityFramework/Entities/LocationApproval.cs
+++ MSR.Infrastructure/Resources/EntityFramework/Entities/LocationApproval.cs
@@ -6,9 +6,9 @@ namespace MSR.Infrastructure.Resources.EntityFramework.Entities
     [Table(nameof(LocationApproval))]
     public partial class LocationApproval: ApprovalEntity
     {
-        public int LocationId { get; set; }
+        public int? LocationId { get; set; }
 
-        public int OldId { get; set; }
+        public int? OldId { get; set; }
 
         [StringLength(100)]
         public string Address1 { get; set; }
