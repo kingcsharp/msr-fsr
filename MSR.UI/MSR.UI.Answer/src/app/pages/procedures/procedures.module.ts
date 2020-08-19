3a38345ad79db974646a70516764baf57454e192
Robert Lara <admin@robertlara.me>
2020-08-19 14:19:24 -0500

    Added Pages and Routing for Procesdures and Monitors directory with components

---
+++ MSR.UI/MSR.UI.Answer/src/app/pages/procedures/procedures.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TableModule } from 'primeng/table';
+import { NewWidgetModule } from '../../layout/new-widget/widget.module';
+import { DialogModule } from 'primeng/dialog';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { UtilsModule } from '../../layout/utils/utils.module';
+import { ProceduresComponent } from './procedures/procedures.component';
+import { TemplatesComponent } from './templates/templates.component';
+import { ProceduretypesComponent } from './proceduretypes/proceduretypes.component';
+import { ProcedureComponent } from './procedure/procedure.component';
+import { ProceduretypeComponent } from './proceduretype/proceduretype.component';
+import { TemplateComponent } from './template/template.component';
+
+export const routes = [
+  { path: '', redirectTo: 'people', pathMatch: 'full' },
+  { path: 'procedures', component: ProceduresComponent, pathMatch: 'full' },
+  { path: 'proceduretypes', component: ProceduretypesComponent, pathMatch: 'full' },
+  { path: 'proceduretemplates', component: TemplatesComponent, pathMatch: 'full' },
+  { path: 'procedure-create', component: ProcedureComponent, pathMatch: 'full' },
+  { path: 'procedure-edit', component: ProcedureComponent, pathMatch: 'full' },
+  { path: 'proceduretype-create', component: ProceduretypeComponent, pathMatch: 'full' },
+  { path: 'proceduretype-edit', component: ProceduretypeComponent, pathMatch: 'full' },
+  { path: 'proceduretemplates-create', component: TemplateComponent, pathMatch: 'full' },
+  { path: 'proceduretemplates-edit', component: TemplateComponent, pathMatch: 'full' }
+];
+
+
+@NgModule({
+  declarations: [ProceduresComponent, TemplatesComponent, ProceduretypesComponent, ProcedureComponent, ProceduretypeComponent, TemplateComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+  ]
+})
+export class ProceduresModule { static routes = routes;}
---
+++ MSR.UI/MSR.UI.Answer/src/app/pages/procedures/proceduretypes/proceduretypes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proceduretypes',
+  templateUrl: './proceduretypes.component.html',
+  styleUrls: ['./proceduretypes.component.scss']
+})
+export class ProceduretypesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
