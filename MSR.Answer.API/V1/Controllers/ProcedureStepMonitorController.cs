6c48b514f7ae0c5a37914e7c091c306577240cdd
David Dombrowsky <davek@6thstreetradio.org>
2020-08-19 12:51:36 -0700

    merged in ProcedureStepMonitorController.cs from generated code

--- MSR.Answer.API/V1/Controllers/ProcedureStepMonitorController.cs
+++ MSR.Answer.API/V1/Controllers/ProcedureStepMonitorController.cs
@@ -24,17 +24,12 @@ namespace MSR.Answer.API.V1.Controllers
             _dispatcher = dispatcher;
         }
 
-        [HttpGet()]
-        [HasPrivilegeApi("RunnableProcedures", EnumPrivilege.CanRead)]
-        [SwaggerResponse(typeof(AuditActionResult<ICollection<ProcedureStepMonitorRequest>>))]
-        public async Task<IActionResult> GetProcedureStepMonitor(int? id)
-        {
-            var ret = await _dispatcher.DispatchAsync(new GetProcedureStepMonitor() {
-                procedureStepMonitorId = id
-            });
-            return ret.ToOkObjectResponse<ICollection<ProcedureStepMonitorRequest>>();
-        }
-
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="body"></param>
+        /// <param name="version"></param>
+        /// <response code="200"></response>
         [HttpPost]
         [HasPrivilegeApi("RunnableProcedures", EnumPrivilege.CanCreate)]
         [SwaggerResponse(typeof(AuditActionResult<ProcedureStepMonitorRequest>))]
@@ -45,6 +40,51 @@ namespace MSR.Answer.API.V1.Controllers
             return ret.ToOkObjectResponse<ProcedureStepMonitorRequest>();
         }
 
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="version"></param>
+        /// <response code="200"></response>
+        [HttpDelete]
+        [Route("/Robert5/msr-api/v1/v{version}/ProcedureStepMonitor/{id}")]
+        [SwaggerResponse(typeof(AuditActionResult))]
+        public virtual IActionResult ProcedureStepMonitorDeactivateProcedureStepMonitor([FromRoute][Required]int? id, [FromRoute][Required]string version)
+        {
+            //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(200, default(AuditActionResult));
+            string exampleJson = null;
+            exampleJson = "{\n  \"errorMessages\" : [ {\n    \"number\" : 0,\n    \"message\" : \"message\",\n    \"isValidationMessage\" : true\n  }, {\n    \"number\" : 0,\n    \"message\" : \"message\",\n    \"isValidationMessage\" : true\n  } ],\n  \"hasValidationErrors\" : true,\n  \"hasErrors\" : true,\n  \"id\" : 6,\n  \"successMessage\" : \"successMessage\"\n}";
+
+                        var example = exampleJson != null
+                        ? JsonConvert.DeserializeObject<AuditActionResult>(exampleJson)
+                        : default(AuditActionResult);            //TODO: Change the data returned
+            return new ObjectResult(example);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="version"></param>
+        /// <param name="id"></param>
+        /// <response code="200"></response>
+        [HttpGet]
+        [HasPrivilegeApi("RunnableProcedures", EnumPrivilege.CanRead)]
+        [SwaggerResponse(typeof(AuditActionResult<ICollection<ProcedureStepMonitorRequest>>))]
+        public async Task<IActionResult> GetProcedureStepMonitor(int? id)
+        {
+            var ret = await _dispatcher.DispatchAsync(new GetProcedureStepMonitor() {
+                procedureStepMonitorId = id
+            });
+            return ret.ToOkObjectResponse<ICollection<ProcedureStepMonitorRequest>>();
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="body"></param>
+        /// <param name="version"></param>
+        /// <response code="200"></response>
         [HttpPatch]
         [HasPrivilegeApi("RunnableProcedures", EnumPrivilege.CanEdit)]
         [SwaggerResponse(typeof(AuditActionResult<ProcedureStepMonitorRequest>))]
@@ -53,15 +93,6 @@ namespace MSR.Answer.API.V1.Controllers
             var command = newproc.ToUpdateProcedureStepMonitorCommand();
             var ret = await _dispatcher.DispatchAsync(command);
             return ret.ToOkObjectResponse<ProcedureStepMonitorRequest>();
-        }
-
-        [HttpGet("definition")]
-        [HasPrivilegeApi("RunnableProcedures", EnumPrivilege.CanRead)]
-        [SwaggerResponse(typeof(AuditActionResult<ProcedureStepMonitorDefinition>))]
-        public async Task<IActionResult> GetProcedureStepMonitorDefinition()
-        {
-            var ret = await _dispatcher.DispatchAsync(new GetProcedureStepMonitorDefinition());
-            return ret.ToOkObjectResponse<ProcedureStepMonitorDefinition>();
         }
     }
 }
