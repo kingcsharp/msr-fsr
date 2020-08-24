276a1b82478c139c0d45325b4dbdf0f2a016af02
Alexandre San <alexandre.sanhaji@cmhworks.com>
2020-08-24 10:21:33 -0700

    A3-26/A3-73 backend ProcedureStepTemplate GET response update (#56)
    * feat: renaming to ProcedureStepTemplateModel, fix ReplacementCost Column Type
    
    * feat: add ProcedureStepTemplateModel LaborTime
    
    * fix: CreateProcedureStepTemplate command data types
    
    * feat: add UsefulLife and LaborTime to ProcedureStepModel

--- MSR.Domain/Commands/CreateProcedureStepTemplate.cs
+++ MSR.Domain/Commands/CreateProcedureStepTemplate.cs
@@ -1,4 +1,5 @@
-﻿using MSR.Domain.Commanding;
+﻿using System;
+using MSR.Domain.Commanding;
 
 namespace MSR.Domain.Commands
 {
@@ -7,10 +8,10 @@ namespace MSR.Domain.Commands
         public string Title { get; set; }
         public string StepText { get; set; }
         public int? SystemTaskId { get; set; }
-        public double? LaborTime { get; set; }
-        public double? EquipmentTime { get; set; }
-        public double? ReplacementCost { get; set; }
-        public double? Utilization { get; set; }
+        public decimal? LaborTime { get; set; }
+        public decimal? EquipmentTime { get; set; }
+        public decimal? ReplacementCost { get; set; }
+        public Single? Utilization { get; set; }
         public int? UsefulLife { get; set; }
         public string Roles { get; set; }
         public string Comments { get; set; }
--- MSR.Domain/Models/ProcedureStepModel.cs
+++ MSR.Domain/Models/ProcedureStepModel.cs
@@ -1,8 +1,7 @@
 ﻿namespace MSR.Domain.Models
 {
-    public class ProcedureStep
+    public class ProcedureStepModel
     {
-        public ProcedureStep() { }
         public int Id { get; set; }
         public int ProcedureId { get; set; }
         public string Title { get; set; }
@@ -11,8 +10,10 @@
         public double Duration { get; set; }
         public string DurationType { get; set; }
         public int PrintOrder { get; set; }
+        public double? LaborTime { get; set; }
         public decimal? ReplacementCost { get; set; }
         public float? Utilization { get; set; }
+        public int? UsefulLife { get; set; }
         public double? EquipmentTime { get; set; }
         public string Roles { get; set; }
     }
