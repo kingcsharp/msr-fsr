398a69572ac0719a66220d76b1dae316b8614c0d
alecellis1985 <alecellis1985@gmail.com>
2020-08-25 04:34:14 -0300

    parts cypress renameing

--- MSR.UI/MSR.UI.Answer/cypress/integration/parts_functionallity/TC73_AddPart.js
+++ MSR.UI/MSR.UI.Answer/cypress/integration/parts_functionallity/TC73_AddPart.js
@@ -1,6 +1,6 @@
 import { ColumnsSaved } from '../../../src/app/models/lib/ColumnsSaved';
 
-describe('Invoice Functionality', () => {
+describe('Parts Functionality', () => {
     it('TC73_AddPart', () => {
         cy.server();
         cy.route({
