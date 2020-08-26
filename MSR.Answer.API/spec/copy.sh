0711926f822a72b074af310537898a74439bc4f3
David Dombrowsky <davek@6thstreetradio.org>
2020-08-26 09:02:14 -0700

    Added helper script for API comparison

---
+++ MSR.Answer.API/spec/copy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Used to adjust the output of the swagger web UI to
+# something that can be compared to the stored spec
+# used to import into assertible.
+
+set -x
+
+cp ~/xfer/swagger.json swagger-work.json
+dos2unix swagger-work.json
+patch -p1 < swagger-removebearer.patch || exit
+patch -p1 < swagger-addexamples.patch || exit
