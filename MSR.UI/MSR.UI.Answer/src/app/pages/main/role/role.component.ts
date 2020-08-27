1401b7ec5d618ca1336f5db944a374be7d4b8b5d
alecellis1985 <alecellis1985@gmail.com>
2020-08-27 13:38:46 -0300

    lint it up more, add to package

--- MSR.UI/MSR.UI.Answer/src/app/pages/main/role/role.component.ts
+++ MSR.UI/MSR.UI.Answer/src/app/pages/main/role/role.component.ts
@@ -78,7 +78,7 @@ export class RoleComponent implements OnInit {
 
   showDialog(roleView: Role) {
     this.currentRole = this.getCurrentRole(roleView);
-    this.availableRoles = this.data.filter((elem) => { return elem.id !== this.currentRole.id; });
+    this.availableRoles = this.data.filter((elem) => elem.id !== this.currentRole.id);
     this.display = true;
   }
 
@@ -92,7 +92,7 @@ export class RoleComponent implements OnInit {
       let data = {
         id: this.currentRole.id,
         name: this.currentRole.name, isCertificationRole: this.currentRole.isCertificationRole,
-        parentRoleIds: this.currentRole.parentRoles.map((role) => { return role.id; })
+        parentRoleIds: this.currentRole.parentRoles.map((role) => role.id)
       };
 
       if (this.currentRole.id !== undefined) {
