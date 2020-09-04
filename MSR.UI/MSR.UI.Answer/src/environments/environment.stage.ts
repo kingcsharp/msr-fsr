a110597b5d9431eca69e617e36d2b2dd08c3f4d1
Tim Millett <>
2020-09-04 17:07:02 -0400

    Update api url

--- MSR.UI/MSR.UI.Answer/src/environments/environment.stage.ts
+++ MSR.UI/MSR.UI.Answer/src/environments/environment.stage.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  url: 'https://stage-api.answer.msr-fsr.com',
-  API_BASE_URL: 'https://stage-api.answer.msr-fsr.com',
+  url: 'https://qa-api.answer.msr-fsr.com',
+  API_BASE_URL: 'https://qa-api.answer.msr-fsr.com',
   hmr: false,
   backend: true,
   version: 'v1',
