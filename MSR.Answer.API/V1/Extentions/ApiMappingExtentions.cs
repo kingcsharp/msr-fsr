7affce6cd5b4ebf5de17f32284cf4a549f6e3e36
Alexandre Sanhaji <alexandre.sanhaji@cmhworks.com>
2020-09-10 13:36:14 -0700

    feat: UpdateAdminCostSettings

--- MSR.Answer.API/V1/Extentions/ApiMappingExtentions.cs
+++ MSR.Answer.API/V1/Extentions/ApiMappingExtentions.cs
@@ -924,5 +924,10 @@ namespace MSR.Answer.API.V1.Extentions
         /// <param name="request"></param>
         /// <returns></returns>
         public static UpdatePurchaseOrder ToUpdatePurchaseOrderCommand(this UpdatePurchaseOrderRequest request) => AutoMapperHelper.Mapper.Map<UpdatePurchaseOrder>(request);
+
+        public static UpdateAdminCostSetting ToUpdateAdminCostSettingsCommand(this UpdateAdminCostSettingRequest request)
+        {
+            return AutoMapperHelper.Mapper.Map<UpdateAdminCostSetting>(request);
+        }
     }
 }
---
+++ MSR.Domain/Commands/UpdateAdminCostSetting.cs
@@ -0,0 +1,14 @@
+﻿using MSR.Domain.Commanding;
+using System;
+using System.Collections.Generic;
+
+namespace MSR.Domain.Commands
+{
+    public class UpdateAdminCostSetting : Command
+    {
+        public decimal? RMAnnualRate { get; set; }
+        public decimal? LaborRateMinute { get; set; }
+        public int? YearsHours { get; set; }
+        public int? HourMinutes { get; set; }
+    }
+}
