f0c7763b26a73aad28e7f83d8481463f137a63e1
Alexandre Sanhaji <alexandre.sanhaji@cmhworks.com>
2020-09-10 13:36:14 -0700

    feat: UpdateAdminCostSettings

--- MSR.Infrastructure/Resources/Services/AdminCostSettingsService.cs
+++ MSR.Infrastructure/Resources/Services/AdminCostSettingsService.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using MSR.Domain.Abstractions.Services;
+using MSR.Domain.Commands;
+using MSR.Domain.Exceptions;
 using MSR.Domain.Models;
 using MSR.Infrastructure.Resources.EntityFramework.Application;
 using MSR.Infrastructure.Resources.EntityFramework.Entities;
@@ -24,6 +26,32 @@ namespace MSR.Infrastructure.Resources.Services.PurchaseOrder
         {
             AdminCostSetting ret = await _unitOfWork.AdminCostSettings.Query().FirstOrDefaultAsync();
             return _mapper.Map<AdminCostSettingsModel>(ret);
+        }
+
+        public async Task<AdminCostSettingsModel> UpdateAdminCostSettingAsync(UpdateAdminCostSetting command)
+        {
+            // Retreive the single AdminCostSetting 
+            var adminCostSetting = await _unitOfWork.AdminCostSettings
+                                .Query()
+                                .SingleOrDefaultAsync();
+
+            if (adminCostSetting is null)
+            {
+                throw new DomainException($"{nameof(AdminCostSetting)} not found");
+            }
+
+            // Update AdminCostSetting values
+            adminCostSetting.RMAnnualRate = command.RMAnnualRate ?? adminCostSetting.RMAnnualRate;
+            adminCostSetting.LaborRateMinute = command.LaborRateMinute ?? adminCostSetting.LaborRateMinute;
+            adminCostSetting.HourMinutes = command.HourMinutes ?? adminCostSetting.HourMinutes;
+            adminCostSetting.YearsHours = command.YearsHours ?? adminCostSetting.YearsHours;
+
+            // Save AdminCostSetting changes
+            await _unitOfWork.AdminCostSettings.UpdateAndSaveChangesAsync(adminCostSetting);
+
+            var retadminCostSetting = _mapper.Map<AdminCostSettingsModel>(adminCostSetting);
+
+            return retadminCostSetting;
         }
     }
 }
