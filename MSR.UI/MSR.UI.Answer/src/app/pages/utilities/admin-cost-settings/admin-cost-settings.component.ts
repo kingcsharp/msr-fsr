a93e1f7f8e492e0791fbc71581c3d433a01ca395
starsoftdev <derek.otieno.2020@gmail.com>
2020-09-13 01:58:21 -0400

    Add admin cost settings page

---
+++ MSR.UI/MSR.UI.Answer/src/app/pages/utilities/admin-cost-settings/admin-cost-settings.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { Globals } from '../../../models/lib/globals';
+import { take } from 'rxjs/operators';
+import { responseHandler } from '../../../utils/responseHandler';
+import { environment as env } from '../../../../environments/environment';
+import { AdminCostSettingsService, AdminCostSettingsModel, } from '../../../services/api.client.generated';
+declare let jQuery: any;
+
+@Component({
+  selector: 'app-admin-cost-settings',
+  templateUrl: './admin-cost-settings.component.html',
+  styleUrls: ['./admin-cost-settings.component.scss'],
+  providers: [
+    AdminCostSettingsService,
+  ]
+})
+export class AdminCostSettingsComponent implements OnInit {
+  adminCostSettings: any;
+  getAdminCostSettingsFlag: boolean = false;
+
+  constructor(
+    public globals: Globals,
+    private adminCostSettingsService: AdminCostSettingsService,
+  ) { }
+
+  ngOnInit(): void {
+    this.getAdminCostSettings();
+  }
+
+  getAdminCostSettings() {
+    this.globals.showLoader(true);
+    this.adminCostSettingsService.adminCostSettings(env.apiVersion)
+      .pipe(take(1))
+      .subscribe(responseHandler(response => {
+        this.adminCostSettings = response.object;
+        this.getAdminCostSettingsFlag = true;
+      }));
+  }
+
+  onSubmit() {
+    jQuery('.parsleyjs').parsley().validate();
+    if (jQuery('.parsleyjs').parsley().isValid()) {
+      const RequestData = {
+        rmAnnualRate: parseFloat(this.adminCostSettings.rmAnnualRate),
+        laborRateMinute: parseFloat(this.adminCostSettings.laborRateMinute),
+        yearsHours: parseInt(this.adminCostSettings.yearsHours),
+        hourMinutes: parseInt(this.adminCostSettings.hourMinutes)
+      }
+      console.log('_____', RequestData)
+      // TODO: PATCH API
+
+    }
+  }
+
+}
---
+++ MSR.UI/MSR.UI.Answer/src/app/pages/utilities/equipment-maintenance/equipment-maintenance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-equipment-maintenance',
+  templateUrl: './equipment-maintenance.component.html',
+  styleUrls: ['./equipment-maintenance.component.scss']
+})
+export class EquipmentMaintenanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
