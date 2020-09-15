230547742ab650fad247a54d0fe5c960e7f4a16b
David Dombrowsky <davek@6thstreetradio.org>
2020-09-15 14:36:35 -0400

    command for get work orders

---
+++ MSR.Domain/Commands/GetWorkOrderView.cs
@@ -0,0 +1,17 @@
+﻿using MSR.Domain.Commanding;
+using MSR.Domain.Models;
+using System.Collections.Generic;
+
+namespace MSR.Domain.Commands
+{
+    /// <summary>
+    /// GetWorkOrderViewRequest
+    /// </summary>
+    public class GetWorkOrderView: Command
+    {
+        /// <summary>
+        /// If true, return only NOT in progress or waiting
+        /// </summary>
+        public bool IsHistory { get; set; }
+    }
+}
