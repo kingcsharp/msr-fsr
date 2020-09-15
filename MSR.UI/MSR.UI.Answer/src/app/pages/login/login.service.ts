f407def0303abbcae59b519fafa4e7f50a3ac27b
alecellis1985 <alecellis1985@gmail.com>
2020-09-15 16:31:08 -0300

    fix login and scrollbar

--- MSR.UI/MSR.UI.Answer/src/app/pages/login/login.service.ts
+++ MSR.UI/MSR.UI.Answer/src/app/pages/login/login.service.ts
@@ -14,7 +14,7 @@ const jwt = new JwtHelperService();
 @Injectable()
 export class LoginService {
   config: any;
-  _isFetching: boolean = false;
+  isFetching: boolean = false;
   _errorMessage: string = '';
 
   constructor(
@@ -24,14 +24,6 @@ export class LoginService {
     private router: Router, private accountService: AccountService, private userService: UserService
   ) {
     this.config = appConfig.getConfig();
-  }
-
-  get isFetching() {
-    return this._isFetching;
-  }
-
-  set isFetching(val: boolean) {
-    this._isFetching = val;
   }
 
   get errorMessage() {
@@ -63,12 +55,13 @@ export class LoginService {
     if (creds.email.length <= 0 || creds.password.length <= 0) {
       this.loginError('Something was wrong. Try again');
     }
-
+    this.isFetching = true;
     this.accountService.login(env.apiVersion, new SystemLoginRequest({ userName: creds.email, password: creds.password }))
       .pipe(take(1))
       .subscribe(responseHandler((result) => {
         ctrl.receiveToken(result.object);
       }, () => {
+        this.isFetching = false;
         ctrl.loginError('Username or Password is invalid.');
       }));
   }
@@ -91,6 +84,7 @@ export class LoginService {
         if (user.roles.length === 0) {
           this.logoutUser();
           this.loginError('Sorry you do not have roles associated with your user.');
+          this.isFetching = false;
           return;
         }
         this.receiveLogin();
@@ -101,6 +95,7 @@ export class LoginService {
     localStorage.removeItem('token');
     localStorage.removeItem('user');
     document.cookie = 'token=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    this.isFetching = false;
     this.router.navigate(['/login']);
   }
 
