ece54097b2539a3c673de6f533adf41b2c0bdbbb
james <james.bridgeford@cmhworks.com>
2020-09-22 21:15:28 -0500

    removed the length requirement to allow for INT searches

--- MSR.Answer.API/V1/Models/GetSearchRequest.cs
+++ MSR.Answer.API/V1/Models/GetSearchRequest.cs
@@ -4,7 +4,7 @@ namespace MSR.Answer.API.V1.Models
 {
     public class GetSearchRequest
     {
-        [Required, MinLength(3)]
+        [Required]
         public string SearchTerm { get; set; }
     }
 }
