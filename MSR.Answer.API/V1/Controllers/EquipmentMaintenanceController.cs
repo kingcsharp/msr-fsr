e830f57e3f94d81afdc55cdafc7060d39cd1b580
Alexandre Sanhaji <alexandre.sanhaji@cmhworks.com>
2020-09-23 20:32:44 -0700

    feat: set privilege access to EM/PM controller

--- MSR.Answer.API/V1/Controllers/EquipmentMaintenanceController.cs
+++ MSR.Answer.API/V1/Controllers/EquipmentMaintenanceController.cs
@@ -19,20 +19,26 @@ namespace MSR.Answer.API.V1.Controllers
 {
     [ApiVersion("1.0")]
     [VersionedRoute("[controller]")]
-#if DEBUG
-    [Microsoft.AspNetCore.Authorization.AllowAnonymous]
-#endif
     public class EquipmentMaintenanceController : BaseApiController
     {
         private const string privilegeApiName = "EquipmentMaintenance";
         private readonly ICommandDispatcher _dispatcher;
 
+        /// <summary>
+        /// Ctor EM/PM
+        /// </summary>
+        /// <param name="dispatcher"></param>
         public EquipmentMaintenanceController(ICommandDispatcher dispatcher)
         {
             _dispatcher = dispatcher;
         }
 
-        [HttpGet]//, HasPrivilegeApi(privilegeApiName, EnumPrivilege.CanRead)]
+        /// <summary>
+        /// Gets EM/PM items. Filter the item using Id.
+        /// </summary>
+        /// <param name="filters"></param>
+        /// <returns></returns>
+        [HttpGet, HasPrivilegeApi(privilegeApiName, EnumPrivilege.CanRead)]
         [SwaggerResponse(HttpStatusCode.OK, typeof(AuditActionResult<IEnumerable<EquipmentMaintenanceModel>>))]
         public async Task<IActionResult> GetEquipmentMaintenance([FromQuery] GetEquipmentMaintenanceRequest filters)
         {
@@ -41,7 +47,12 @@ namespace MSR.Answer.API.V1.Controllers
             return ret.ToOkObjectResponse<IEnumerable<EquipmentMaintenanceModel>>();
         }
 
-        [HttpPost]//, HasPrivilegeApi(privilegeApiName, EnumPrivilege.CanCreate)]
+        /// <summary>
+        /// Creates a new EM/PM entry
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [HttpPost, HasPrivilegeApi(privilegeApiName, EnumPrivilege.CanCreate)]
         [SwaggerResponse(HttpStatusCode.OK, typeof(AuditActionResult<EquipmentMaintenanceModel>))]
         public async Task<IActionResult> CreateEquipmentMaintenance([FromBody, Required] CreateEquipmentMaintenanceRequest request)
         {
@@ -50,7 +61,7 @@ namespace MSR.Answer.API.V1.Controllers
             return ret.ToOkObjectResponse<EquipmentMaintenanceModel>("Equipment maintenance has been successfully created.");
         }
 
-        [HttpPatch]//, HasPrivilegeApi(privilegeApiName, EnumPrivilege.CanEdit)]
+        [HttpPatch, HasPrivilegeApi(privilegeApiName, EnumPrivilege.CanEdit)]
         [SwaggerResponse(HttpStatusCode.OK, typeof(AuditActionResult<EquipmentMaintenanceModel>))]
         public async Task<IActionResult> UpdateEquipmentMaintenance([FromBody, Required] UpdateEquipmentMaintenanceRequest request)
         {
