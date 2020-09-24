18879d512a53cadefd0787bd0608ff73fa4fc6e5
alecellis1985 <alecellis1985@gmail.com>
2020-09-24 14:55:22 -0300

    add litener

--- MSR.UI/MSR.UI.Answer/src/app/app.component.ts
+++ MSR.UI/MSR.UI.Answer/src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent {
       setTimeout(() => {
         this.signalrService.startConnection();
         this.signalrService.addWorkflowNotificationListener();
+        this.signalrService.addToasterMessageNotificationListener();
       }, 1000);
     }
   }
