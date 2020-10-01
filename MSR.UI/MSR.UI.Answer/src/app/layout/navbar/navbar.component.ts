d008ea04c69f1799bef8fb7090c4a66760d52b2a
alecellis1985 <alecellis1985@gmail.com>
2020-09-30 23:18:55 -0300

    avatar shows if user image does not load

--- MSR.UI/MSR.UI.Answer/src/app/layout/navbar/navbar.component.ts
+++ MSR.UI/MSR.UI.Answer/src/app/layout/navbar/navbar.component.ts
@@ -20,7 +20,8 @@ export class Navbar implements OnInit {
   settings: any = {
     isOpen: false
   };
-  searchValue:string;
+  searchValue: string;
+  hideImg: false;
 
   constructor(
     private renderer: Renderer2,
@@ -36,6 +37,11 @@ export class Navbar implements OnInit {
   ngOnInit(): void {
     this.searchValue = '';
     this.notificationservice.getNotifications();
+  }
+
+  updateUrl(ev) {
+    console.log(ev);
+    this.hideImg = true;
   }
 
   sidebarPosition(position): void {
@@ -62,9 +68,9 @@ export class Navbar implements OnInit {
   }
 
   search() {
-    if(this.searchValue.length<3){
+    if (this.searchValue.length < 3) {
       this.toastr.error("Search value is too short. It should have 3 characters or more.");
-    }else{
+    } else {
       this.router.navigate(['app/search'], { queryParams: { search: this.searchValue }, queryParamsHandling: 'merge' });
     }
   }
