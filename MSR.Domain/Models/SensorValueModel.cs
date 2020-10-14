f7a8d37d02a76f8f328468654ab9c618822de44d
Robert Lara <admin@robertlara.me>
2020-10-14 03:40:52 -0500

    Added files because yeah, all I do is commits in and Angular app bro!

---
+++ MSR.Domain/Models/SensorValueModel.cs
@@ -0,0 +1,15 @@
+﻿using MSR.Domain.Models.BaseModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSR.Domain.Models
+{
+    public class SensorValueModel: CreatableModel
+    {
+        public SensorModel Sensor { get; set; }
+        public string ItemCurrentValue { get; set; }
+        public string AlarmDescription { get; set; }
+        public bool? IsAlarm { get; set; }
+    }
+}
