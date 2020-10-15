d1216eb752c5892912ea47dc5b74ea32d497a364
David Dombrowsky <davek@6thstreetradio.org>
2020-10-15 16:07:58 -0400

    removed debug code

--- MSR.Infrastructure/Resources/Services/Part/WorkOrderService.cs
+++ MSR.Infrastructure/Resources/Services/Part/WorkOrderService.cs
@@ -758,19 +758,6 @@ namespace MSR.Infrastructure.Resources.Services.Part
 
         public async Task<ICollection<WorkOrderStatus>> GetWorkOrderStatusAsync(GetWorkOrderStatus command)
         {
-// HACK
-// HACK
-// HACK
-// HACK
-// HACK
-ICollection<WorkOrderStatus> rrrret = new List<WorkOrderStatus>();
-return rrrret;
-// HACK
-// HACK
-// HACK
-// HACK
-// HACK
-
             var gwo = _mapper.Map<GetWorkOrder>(command);
 
             gwo.completedOnly = false;
