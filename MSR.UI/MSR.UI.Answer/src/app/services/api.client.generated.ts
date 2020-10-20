b9e02eadd38a4f4db1667f9396cd7042258a1b22
starsoftdev <derek.otieno.2020@gmail.com>
2020-10-20 05:40:51 -0400

    generate api.client.generated.ts

--- MSR.UI/MSR.UI.Answer/src/app/services/api.client.generated.ts
+++ MSR.UI/MSR.UI.Answer/src/app/services/api.client.generated.ts
@@ -15876,7 +15876,7 @@ export interface IWorkOrderTaskMonitorModel extends ITrackableModel {
 export class ProductStepModel implements IProductStepModel {
     id?: number;
     productId?: number;
-    procedureStepId?: number;
+    procedureStepId?: number | undefined;
     laborMinutes?: number | undefined;
     equipmentMinutes?: number | undefined;
     replacementCost?: number | undefined;
@@ -15950,7 +15950,7 @@ export class ProductStepModel implements IProductStepModel {
 export interface IProductStepModel {
     id?: number;
     productId?: number;
-    procedureStepId?: number;
+    procedureStepId?: number | undefined;
     laborMinutes?: number | undefined;
     equipmentMinutes?: number | undefined;
     replacementCost?: number | undefined;
@@ -16067,7 +16067,7 @@ export interface ICreateProductRequest {
 
 export class ProductStep implements IProductStep {
     productId?: number | undefined;
-    procedureStepId!: number;
+    procedureStepId?: number | undefined;
     laborMinutes!: number;
     equipmentMinutes?: number | undefined;
     replacementCost?: number | undefined;
@@ -16132,7 +16132,7 @@ export class ProductStep implements IProductStep {
 
 export interface IProductStep {
     productId?: number | undefined;
-    procedureStepId: number;
+    procedureStepId?: number | undefined;
     laborMinutes: number;
     equipmentMinutes?: number | undefined;
     replacementCost?: number | undefined;
