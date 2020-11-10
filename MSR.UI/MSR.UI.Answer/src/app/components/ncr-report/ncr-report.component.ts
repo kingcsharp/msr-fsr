41c3f9ebd4a661918f035b8abda39b89c54cd942
alecellis1985 <alecellis1985@gmail.com>
2020-11-09 21:47:49 -0300

    create a pipe to show the monitor result so it can be used again anywheere we want

--- MSR.UI/MSR.UI.Answer/src/app/components/ncr-report/ncr-report.component.ts
+++ MSR.UI/MSR.UI.Answer/src/app/components/ncr-report/ncr-report.component.ts
@@ -25,28 +25,20 @@ export class NcrReportComponent implements OnInit {
   constructor(private globals: Globals) { }
 
   ngOnInit(): void {
-
     this.technicianFullName = this.globals.getCurrentUser().fullName;
     this.date = new Date();
-
     this.WorkOrder.workOrderTasks.map(s => {
-
       if (s.workOrderTaskMonitors === null || s.workOrderTaskMonitors === undefined) {
         s.workOrderTaskMonitors = new Array<WorkOrderTaskMonitorModel>();
       }
     });
-
     this.generateMonitorSummaries();
-
     this.workOrderPart = this.WorkOrder.workOrderParts[0];
   }
 
   generateMonitorSummaries() {
-
     this.WorkOrder.workOrderTasks.forEach(workOrderTask => {
-
       if (workOrderTask.procedureStep?.procedure?.procedureTypeId === EnumProcedureType.NCR) {
-
         let taskSummary = {
           taskName: workOrderTask.procedureStep.title,
           taskId: workOrderTask.id,
@@ -54,32 +46,22 @@ export class NcrReportComponent implements OnInit {
         };
 
         workOrderTask.workOrderTaskMonitors.forEach(workOrderTaskMonitor => {
-
           taskSummary.monitors.push({
             monitorTitle: workOrderTaskMonitor.procedureStepMonitor?.description,
-            result: workOrderTaskMonitor.textVal,
+            result: workOrderTaskMonitor,
             comment: workOrderTaskMonitor.comment
-
           });
-
         });
 
         this.taskSummaries.push(taskSummary);
-
-
         workOrderTask.referenceFiles.map(referenceFile => {
-
           if (this.griFileViewerComponent.getViewerType(referenceFile.contentType) === 'img') {
             this.associatedDigitalPictures.push(referenceFile);
           } else {
             this.associatedDocuments.push(referenceFile);
           }
-
         });
-
       }
-
-
     });
   }
 
