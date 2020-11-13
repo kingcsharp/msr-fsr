472de9d2284420802986e205493af6dbdf97d7eb
Robert Lara <admin@robertlara.me>
2020-11-13 11:32:45 -0600

    Fixed bug where drop-down validators were activated based on previous tab of monitors that were shown

--- MSR.UI/MSR.UI.Answer/src/app/components/workordertaskmonitos-wrapper/workordertaskmonitos-wrapper.component.ts
+++ MSR.UI/MSR.UI.Answer/src/app/components/workordertaskmonitos-wrapper/workordertaskmonitos-wrapper.component.ts
@@ -82,6 +82,10 @@ export class WorkordertaskmonitosWrapperComponent implements OnInit {
 
   }
 
+  ngOnChanges(changes) {
+    this.wasValidationCalled = false;
+  }
+
   areDropDownsValid(): boolean {
 
     let dropDownsAreValid = true;
