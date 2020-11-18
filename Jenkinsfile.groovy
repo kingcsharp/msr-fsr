1b1d63df7c1622f080290742c6c2505581f23af6
Tim Millett <>
2020-11-17 22:31:08 -0500

    Update processor container

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -347,7 +347,7 @@ void deploy_processor(composeFile,name,target, app) {
         //sh "ecs-cli configure profile --access-key ${AWS_ACCESS_KEY_ID} --secret-key ${AWS_SECRET_ACCESS_KEY} --profile-name answer-profile"
         sh "ecs-cli configure profile --access-key AKIAWGEEZQATRVZEHMGB --secret-key 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 --profile-name answer-profile"
 
-        sh "ecs-cli compose --file ${composeFile} --project-name ${name}-processor service up --create-log-groups --cluster-config answer-config --ecs-profile answer-profile --container-name ${app} --container-port 80 --timeout 15"
+        sh "ecs-cli compose --file ${composeFile} --project-name ${name}-processor service up --create-log-groups --cluster-config answer-config --ecs-profile answer-profile --timeout 15"
         //sh "ecs-cli compose --file ${composeFile} --project-name ${name} --cluster-config answer-config --ecs-profile answer-profile service scale 0 -—timeout 15"
 
     }
