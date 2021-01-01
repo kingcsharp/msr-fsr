0f27e5a7baa46c151144b561cb8feeaf03dfd0f3
Tim Millett <>
2021-01-01 11:11:52 -0500

    Update

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -99,13 +99,22 @@ pipeline {
                         script {
                             try {
                                 dir('reverseproxy') {
-                                    //sh "sudo chmod 777 /var/run/docker.sock"
-                                    echo "Building server container...."
+                                    echo "Building api proxy container...."
                                     sh "docker build --build-arg NGINX_CONF=dev -t msr-rp ."
                                     sh "docker tag msr-rp ${ACCOUNT_URL}/msr-rp:${env.GIT_COMMIT}"
 
                                     sh "eval \$(/snap/bin/aws ecr get-login --region ${REGION} --no-include-email ${PROFILE} | sed 's|https://||')"
                                     sh "docker push ${ACCOUNT_URL}/msr-rp:${env.GIT_COMMIT}"
+                                }
+
+                                dir('messageproxy') {
+                                    echo "Building message proxy container...."
+
+                                    sh "docker build --build-arg NGINX_CONF=dev -t msr-mp ."
+                                    sh "docker tag msr-mp ${ACCOUNT_URL}/msr-mp:${env.GIT_COMMIT}"
+
+                                    sh "eval \$(/snap/bin/aws ecr get-login --region ${REGION} --no-include-email ${PROFILE} | sed 's|https://||')"
+                                    sh "docker push ${ACCOUNT_URL}/msr-mp:${env.GIT_COMMIT}"
                                 }
                             } catch(e) {
                                 office365ConnectorSend color: "${RED}", message: "${env.BRANCH_NAME} build FAILED building the NGINX image. \n Error: ${e}", status: 'Failed', webhookUrl: "${WEBHOOK_URL}"
@@ -114,8 +123,6 @@ pipeline {
                             }
 
                             try {
-                                //sh "sudo chmod 777 /var/run/docker.sock"
-                                //sh "git mv Msr.Infrastructure MSR.Infrastructure"
                                 echo "Building API container...."
                                 sh "docker build -f MSR.Answer.API/Dockerfile -t msr-api ."
                                 sh "docker tag msr-api ${ACCOUNT_URL}/msr-api:${env.GIT_COMMIT}"
