ecf9f99257878fb09a45b38c2696c869c467f7c0
Tim Millett <>
2021-01-18 00:47:15 -0500

    Update

--- update_image_api.sh
+++ update_image_api.sh
@@ -7,6 +7,7 @@
 sed -i 's/msr-api:.*/msr-api:'"$2"'/' $3
 sed -i 's/msr-processor:.*/msr-processor:'"$2"'/' $3
 sed -i 's/msr-rp:.*/msr-rp:'"$2"'/' $3
+sed -i 's/msr-mp:.*/msr-mp:'"$2"'/' $3
 sed -i 's/ASPNETCORE_ENVIRONMENT=.*/ASPNETCORE_ENVIRONMENT='"$1"'/' $3
 sed -i 's/answer3-api-.*/answer3-api-'"$1"'/' $3
 sed -i 's/answer3-processor-.*/answer3-processor-'"$1"'/' $3
