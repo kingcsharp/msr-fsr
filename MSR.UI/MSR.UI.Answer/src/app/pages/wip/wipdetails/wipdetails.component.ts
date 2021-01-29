47ad945119cc68923d1530162c20401d3e2e3cdb
Robert Lara <admin@robertlara.me>
2021-01-28 20:01:56 -0600

    Added logic to render WIP details when procedure step is not present and ignore checking role access in historical view

--- MSR.UI/MSR.UI.Answer/src/app/pages/wip/wipdetails/wipdetails.component.ts
+++ MSR.UI/MSR.UI.Answer/src/app/pages/wip/wipdetails/wipdetails.component.ts
@@ -162,7 +162,9 @@ export class WipdetailsComponent implements OnInit {
 
     workOrderModel.workOrderTasks.map(workOrderTask => {
 
-      // procedureStepGetRequests.push(this.procedureService.stepGet(workOrderTask.procedureStep?.procedureId, workOrderTask.procedureStep?.id, env.apiVersion));
+      if(workOrderTask.procedureStep !== undefined){
+        procedureStepGetRequests.push(this.procedureService.stepGet(workOrderTask.procedureStep?.procedureId, workOrderTask.procedureStep?.id, env.apiVersion));
+      }
 
     });
 
@@ -170,7 +172,7 @@ export class WipdetailsComponent implements OnInit {
 
       let procedureStepModels = <Array<ProcedureStepModel>>responses.map(s => s.object[0]);
 
-      workOrderModel.workOrderTasks.map(workOrderTask => {
+      workOrderModel.workOrderTasks?.filter(s => s.procedureStep !== undefined).map(workOrderTask => {
 
         let procedureStepModel = procedureStepModels.find(s => s.id === workOrderTask.procedureStep.id);
         workOrderTask.procedureStep.referenceDocumentIds = procedureStepModel.referenceDocumentIds;
@@ -190,7 +192,7 @@ export class WipdetailsComponent implements OnInit {
       forkJoin(documentRequests).subscribe(responseHandler(documentResponses => {
 
         let documents = documentResponses.map(s => s.object[0]);
-        workOrderModel.workOrderTasks.map(workOrderTask => {
+        workOrderModel.workOrderTasks.filter(s => s.procedureStep !== undefined).map(workOrderTask => {
 
           workOrderTask.procedureStep.referenceDocument = new Array<FileModel>();
 
@@ -212,18 +214,28 @@ export class WipdetailsComponent implements OnInit {
 
   checkRoleAccessAndSetTaskAsViewable(workOrderTask: WorkOrderTaskModel) {
 
-    if (this.canUserAccessWorkOrderTask(workOrderTask)) {
+    if (this.canUserAccessWorkOrderTask(workOrderTask) && workOrderTask.procedureStep !== undefined) {
       this.workOrderTaskInProgress = workOrderTask;
       this.workOrderTaskToView = workOrderTask;
       this.rolesRequiredToViewTask.length = 0;
       this.hasAccessToTaskBeingViewed = true;
       this.rolesRequiredMessage = undefined;
-    } else {
-      this.nameOfTaskThatIsRestricted = workOrderTask.procedureStep.title;
+    } 
+    
+    if(workOrderTask.procedureStep !== undefined && !this.canUserAccessWorkOrderTask(workOrderTask)) {
+      this.nameOfTaskThatIsRestricted = workOrderTask.procedureStep === undefined ? workOrderTask.title : workOrderTask.procedureStep?.title;
       this.generateRolesRequiredMessage(workOrderTask.procedureStep.roles.map(s => s.name));
       this.hasAccessToTaskBeingViewed = false;
       this.workOrderTaskInProgress = workOrderTask;
       this.workOrderTaskToView = workOrderTask;
+    }
+
+    if(workOrderTask.procedureStep === undefined) {
+      this.workOrderTaskInProgress = workOrderTask;
+      this.workOrderTaskToView = workOrderTask;
+      this.rolesRequiredToViewTask.length = 0;
+      this.hasAccessToTaskBeingViewed = true;
+      this.rolesRequiredMessage = undefined;
     }
 
   }
