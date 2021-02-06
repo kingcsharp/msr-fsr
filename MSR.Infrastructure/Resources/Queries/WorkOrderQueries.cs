38d461c38894ea10b982676b4dfa875abf2c8b03
james <james.bridgeford@cmhworks.com>
2021-02-06 13:07:41 -0600

    fixing enum return

--- MSR.Infrastructure/Resources/Queries/WorkOrderQueries.cs
+++ MSR.Infrastructure/Resources/Queries/WorkOrderQueries.cs
@@ -80,7 +80,7 @@ namespace MSR.Infrastructure.Resources.Queries
 
             Parallel.ForEach(workOrderStatusDTOs, workOrderStatusDTO =>
             {
-                var status = GetWorkOrderStatusFromTasks(workOrderStatusDTO.WorkOrderTasks);
+                var status = EnumUtils.GetDescription(GetWorkOrderStatusFromTasks(workOrderStatusDTO.WorkOrderTasks));
                 var firstTask = workOrderStatusDTO.WorkOrderTasks.FirstOrDefault(i => i.StatusId == 3 || i.StatusId == 6 || i.StatusId == 8);
 
                 var assignedToUser = firstTask == null ? workOrderStatusDTO.WorkOrderTasks.Any() ? workOrderStatusDTO.WorkOrderTasks.First().AssignedToUser : string.Empty : firstTask.AssignedToUser;
@@ -102,7 +102,7 @@ namespace MSR.Infrastructure.Resources.Queries
                         WorkOrderItemNumber = workOrderStatusDTO.WorkOrderItemNumber,
                         WorkOrderPartSerialNumber = workOrderStatusDTO.WorkOrderPart?.SerialNumber,
                         WorkOrderScheduledEndDate = workOrderStatusDTO.WorkOrderScheduledEndDate,
-                        WorkOrderStatus = status.ToString()
+                        WorkOrderStatus = status
                     }
                 };
 
