b51d6f0c2c1506e3043f78566be6e2f137da7f49
starsoftdev <derek.otieno.2020@gmail.com>
2021-02-16 14:24:22 -0500

    remove nullable from string value

--- MSR.Domain/Models/PartImportItem.cs
+++ MSR.Domain/Models/PartImportItem.cs
@@ -5,8 +5,8 @@ namespace MSR.Domain.Models
         public int? Id { get; set; }
         public string Name { get; set; }
         public string PartNumber { get; set; }
-        public string? OEMPartNumber { get; set; }
-        public string? NickName { get; set; }
+        public string OEMPartNumber { get; set; }
+        public string NickName { get; set; }
         public int? MaximumCycles { get; set; }
     }
 }
