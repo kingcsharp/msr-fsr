55c53c9889c1b2bb5ef4c6aa0d5ea02c99f959f5
Tim Millett <>
2021-02-18 18:11:20 -0500

    Codebuild portal

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -48,8 +48,9 @@ pipeline {
                                     awsSecretKey: 'L8uOFI6V3bbVnRdOBkIQQTSqgY8nnYiHQs+M+gQc',
                                     projectName: 'answer-ui',
                                     region: "us-west-2",
-                                    sourceControlType: "project",
-                                    buildSpecFile: 'Answer3.0/MSR.UI/MSR.UI.Answer/buildspec-answer-ui-qa.yml'
+                                    //sourceControlType: "project",
+                                    sourceControlType: 'jenkins',
+                                    buildSpecFile: 'MSR.UI/MSR.UI.Answer/buildspec-answer-ui-qa.yml'
                                     //sourceLocationOverride: "git@github.com:MSR-FSR/Answer3.0.git",
                                     //sourceTypeOverride: "GITHUB",
                                     //sourceVersion: "nodes"
