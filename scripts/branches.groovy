e1a52dc2ce08d07fcf433b59216a73b466792369
Tim Millett <>
2021-02-20 11:36:28 -0500

    test github pr

--- scripts/branches.groovy
+++ scripts/branches.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('build') {
             steps {
-                try {
+                //try {
                     dir('reverseproxy') {
                         echo "Building api proxy container...."
                         sh "docker build --build-arg NGINX_CONF=dev -t msr-rp ."
@@ -12,13 +12,13 @@ pipeline {
                         sh "eval \$(/snap/bin/aws ecr get-login --region ${REGION} --no-include-email ${PROFILE} | sed 's|https://||')"
                         sh "docker push ${ACCOUNT_URL}/msr-rp:${env.GIT_COMMIT}"
                     }
-                } catch(e) {
-                    office365ConnectorSend color: "${RED}", message: "${env.BRANCH_NAME} build FAILED building the NGINX image. \n Error: ${e}", status: 'Failed', webhookUrl: "${WEBHOOK_URL}"
-                    currentBuild.result = 'FAILURE'
-                    sh "exit 1"
-                }
+//                } catch(e) {
+//                    office365ConnectorSend color: "${RED}", message: "${env.BRANCH_NAME} build FAILED building the NGINX image. \n Error: ${e}", status: 'Failed', webhookUrl: "${WEBHOOK_URL}"
+//                    currentBuild.result = 'FAILURE'
+//                    sh "exit 1"
+//                }
 
-                try {
+                //try {
                     echo "Building API container...."
                     sh "docker build -f MSR.Answer.API/Dockerfile -t msr-api ."
                     sh "docker tag msr-api ${ACCOUNT_URL}/msr-api:${env.GIT_COMMIT}"
@@ -26,11 +26,11 @@ pipeline {
                     sh "eval \$(/snap/bin/aws ecr get-login --region ${REGION} --no-include-email ${PROFILE} | sed 's|https://||')"
                     sh "docker push ${ACCOUNT_URL}/msr-api:${env.GIT_COMMIT}"
 
-                } catch(e) {
-                    office365ConnectorSend color: "${RED}", message: "${env.BRANCH_NAME} build FAILED building the API image. \n Error: ${e}", status: 'Failed', webhookUrl: "${WEBHOOK_URL}"
-                    currentBuild.result = 'FAILURE'
-                    sh "exit 1"
-                }
+//                } catch(e) {
+//                    office365ConnectorSend color: "${RED}", message: "${env.BRANCH_NAME} build FAILED building the API image. \n Error: ${e}", status: 'Failed', webhookUrl: "${WEBHOOK_URL}"
+//                    currentBuild.result = 'FAILURE'
+//                    sh "exit 1"
+//                }
             }
         }
     }
