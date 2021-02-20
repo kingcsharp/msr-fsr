1bcc36e74004f5804d93e6d9ce13296d9afcf43d
Tim Millett <>
2021-02-20 11:41:11 -0500

    test github pr

--- scripts/branches.groovy
+++ scripts/branches.groovy
@@ -37,10 +37,6 @@ pipeline {
                     dir('reverseproxy') {
                         echo "Building api proxy container...."
                         sh "docker build --build-arg NGINX_CONF=dev -t msr-rp ."
-                        sh "docker tag msr-rp ${ACCOUNT_URL}/msr-rp:${env.GIT_COMMIT}"
-
-                        sh "eval \$(/snap/bin/aws ecr get-login --region ${REGION} --no-include-email ${PROFILE} | sed 's|https://||')"
-                        sh "docker push ${ACCOUNT_URL}/msr-rp:${env.GIT_COMMIT}"
                     }
 //                } catch(e) {
 //                    office365ConnectorSend color: "${RED}", message: "${env.BRANCH_NAME} build FAILED building the NGINX image. \n Error: ${e}", status: 'Failed', webhookUrl: "${WEBHOOK_URL}"
@@ -51,10 +47,7 @@ pipeline {
                 //try {
                     echo "Building API container...."
                     sh "docker build -f MSR.Answer.API/Dockerfile -t msr-api ."
-                    sh "docker tag msr-api ${ACCOUNT_URL}/msr-api:${env.GIT_COMMIT}"
 
-                    sh "eval \$(/snap/bin/aws ecr get-login --region ${REGION} --no-include-email ${PROFILE} | sed 's|https://||')"
-                    sh "docker push ${ACCOUNT_URL}/msr-api:${env.GIT_COMMIT}"
 
 //                } catch(e) {
 //                    office365ConnectorSend color: "${RED}", message: "${env.BRANCH_NAME} build FAILED building the API image. \n Error: ${e}", status: 'Failed', webhookUrl: "${WEBHOOK_URL}"
