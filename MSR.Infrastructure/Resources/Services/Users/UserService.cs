caf460f18d532849f0c25ace2f49133c7727acb9
David Dombrowsky <davek@6thstreetradio.org>
2021-03-03 20:54:40 -0500

    the certification list in 'User' path must return a list, not enumerable

--- MSR.Infrastructure/Resources/Services/Users/UserService.cs
+++ MSR.Infrastructure/Resources/Services/Users/UserService.cs
@@ -595,7 +595,7 @@ namespace MSR.Infrastructure.Resources.Services.Users
                 userRoles = userRoles.Where(i => i.UserId == command.Id.Value);
             }
 
-            return await Task.FromResult(userRoles
+            return await userRoles
                 .Include(i => i.User)
                 .Include(i => i.Role)
                 .Where(i => i.Role.IsCertificationRole.HasValue && i.Role.IsCertificationRole.Value)
@@ -607,8 +607,7 @@ namespace MSR.Infrastructure.Resources.Services.Users
                         Status = (i.CertificationToDate.HasValue ? DateTime.Compare(i.CertificationToDate.Value, DateTime.UtcNow) <= 0 ? "Expired" : "Active" : "Active"),
                         CertificationName = i.Role.Name
                     }
-                ).AsEnumerable()
-            );
+                ).ToListAsync();
         }
     }
 }
