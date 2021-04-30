75adde9ecf2b41bb880cca23bdff42e2af6d282f
David Dombrowsky <davek@6thstreetradio.org>
2021-04-30 12:11:10 -0400

    moved timeout exception strings to constants

--- MSR.Domain/Commanding/CommandDispatcher.cs
+++ MSR.Domain/Commanding/CommandDispatcher.cs
@@ -9,6 +9,8 @@ namespace MSR.Domain.Commanding
 {
     public class CommandDispatcher : ICommandDispatcher
     {
+        public const string EXECUTION_TIMEOUT_EXPIRED = "EXECUTION TIMEOUT EXPIRED";
+        public const string TIMEOUT_EXPIRED = "TIMEOUT EXPIRED";
         private readonly IServiceProvider _serviceProvider;
         private readonly ILogger _logger;
 
@@ -38,11 +40,11 @@ namespace MSR.Domain.Commanding
             }
             catch (Exception ex)
             {
-                if (ex.Message.ToUpper().StartsWith("EXECUTION TIMEOUT EXPIRED") ||
-                    ex.Message.ToUpper().StartsWith("TIMEOUT EXPIRED") ||
+                if (ex.Message.ToUpper().StartsWith(EXECUTION_TIMEOUT_EXPIRED) ||
+                    ex.Message.ToUpper().StartsWith(TIMEOUT_EXPIRED) ||
                     (ex.InnerException != null &&
                      ex.InnerException.Message
-                       .ToUpper().StartsWith("EXECUTION TIMEOUT EXPIRED")))
+                       .ToUpper().StartsWith(EXECUTION_TIMEOUT_EXPIRED)))
                 {
                     return CommandResponse.Retry(ex);
                 }
