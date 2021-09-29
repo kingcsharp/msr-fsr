c4af714ce7ddad310fb77078d5579ee9aa54683b
JamesBridgeford <26014782+JamesBridgeford@users.noreply.github.com>
2021-09-28 21:18:51 -0500

    added a router subscription to modify the url for QRCode when someone changes the url manually. (#661)

--- MSR.UI/MSR.UI.Answer/src/app/components/printtraveler-report/printtraveler-report.component.ts
+++ MSR.UI/MSR.UI.Answer/src/app/components/printtraveler-report/printtraveler-report.component.ts
@@ -2,7 +2,9 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { WorkOrderModel, WorkOrderPartModel, WorkOrderPartService } from '../../services/api.client.generated';
 import { environment as env } from '../../../environments/environment';
 import { responseHandler } from '../../utils/responseHandler';
-import { take } from 'rxjs/operators';
+import { filter, take } from 'rxjs/operators';
+import { Router,  NavigationEnd } from '@angular/router';
+
 
 @Component({
   selector: 'printtraveler-report',
@@ -19,13 +21,18 @@ export class PrinttravelerReportComponent implements OnInit {
   urlToWipDetailsPage: string;
   parentPart: WorkOrderPartModel;
 
-  constructor(private workOrderPartService: WorkOrderPartService) { }
+  constructor(private workOrderPartService: WorkOrderPartService, private router: Router) { }
 
   ngOnInit(): void {
+    this.router.events.pipe(
+      filter.call(
+        this.router.events, 
+        (event:Event) => event instanceof NavigationEnd
+      )).subscribe(x => {this.urlToWipDetailsPage = window.location.href});
 
-    this.urlToWipDetailsPage = window.location.href;
+      this.urlToWipDetailsPage = window.location.href;
 
-    this.workOrderPartService.workOrderPartGet(this.WorkOrder.workOrderParts[0].id, null, env.apiVersion).pipe(take(1)).subscribe(responseHandler(response => {
+      this.workOrderPartService.workOrderPartGet(this.WorkOrder.workOrderParts[0].id, null, env.apiVersion).pipe(take(1)).subscribe(responseHandler(response => {
       this.parentPart = response.object[0];
       this.parentPartImageUrl = this.parentPart?.part?.files[0]?.fileURL;
     }));
