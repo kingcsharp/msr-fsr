5a9a5ddffc537cba65851623de734639154f02d7
JamesBridgeford <26014782+JamesBridgeford@users.noreply.github.com>
2022-03-21 22:26:48 -0500

    changing remote url (#713)
    * changing remote url
    
    * updating credentialsId
    
    Co-authored-by: jamesbridgeford <james.bridgeford@protonmail.ch>

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,8 @@
+
 library identifier: 'build-configs@master',
 
-        retriever: modernSCM([$class: 'GitSCMSource', credentialsId: '7d49a174-7aba-4ac4-88e6-489ab13a4156',
-                              remote: 'git@github.com:CMHWorks/build-configs.git',
+        retriever: modernSCM([$class: 'GitSCMSource', credentialsId: 'b7bf4905-1bd1-47f1-8ea3-2478c05e2622',
+                              remote: 'https://github.com/CMHWorks/build-configs.git',
                               traits: [[$class: 'WipeWorkspaceTrait']]])
 
 build("answer")
