fa3c12a804f69403bd4565ef577753580ee3e114
jamesbridgeford <26014782+JamesBridgeford@users.noreply.github.com>
2022-08-10 14:18:55 -0500

    Fixing bad reference

--- MSR.Infrastructure/Resources/Services/Purchase/PurchaseService.cs
+++ MSR.Infrastructure/Resources/Services/Purchase/PurchaseService.cs
@@ -106,7 +106,7 @@ namespace MSR.Infrastructure.Resources.Services.PurchaseOrder
                     await created.Context.Entry(purchase)
                         .Reference(x => x.PurchaseOrder).LoadAsync();
                     await created.Context.Entry(purchase)
-                        .Reference(x => x.PurchaseProducts).LoadAsync();
+                        .Collection(x => x.PurchaseProducts).LoadAsync();
 
                     var purchaseModel = _mapper.Map<PurchaseModel>(purchase);
                     purchaseModel.SerializeIndividually = command.PurchaseRequests.First().SerializeIndividually;
