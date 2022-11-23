252ab5f9490768bdfd97e3d5932311160f2891d6
Jared Mahan <jaredmahan@gmail.com>
2022-11-23 16:39:32 -0600

    chore: Fix issues with csv import

--- MSR.Domain/Models/QuoteImportItem.cs
+++ MSR.Domain/Models/QuoteImportItem.cs
@@ -9,11 +9,10 @@ namespace MSR.Domain.Models
         public string CustomerName { get; set; }
         public string DivisionFab { get; set; }
         public int PartKitNo { get; set; }
-        public EnumSegregationType SegregationType { get; set; }
         public int ProcedureId { get; set; }
-        public string ProductName { get; set; }
+        public string Name { get; set; }
         public int Revision { get; set; }
-        public decimal TotalSalesPrice { get; set; }
+        public decimal TotalSalePrice { get; set; }
         public int CycleTime { get; set; }
     }
 }
