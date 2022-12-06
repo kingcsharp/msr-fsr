700ba07697831e9e1068f7a0825053fd809cecd7
starsoftdev <derek.otieno.2020@gmail.com>
2022-12-06 10:36:23 -0500

    display total values for all pages

--- MSR.UI/MSR.UI.Answer/src/app/models/paging-model.ts
+++ MSR.UI/MSR.UI.Answer/src/app/models/paging-model.ts
@@ -5,6 +5,8 @@ export interface IPagingModel {
     pageNumber: number;
     pageSize: number;
     totalRows: number;
+    totals: number;
+    showTotals: boolean;
     data: Array<any>;
     HighChartsOptions: Options;
     ChartInformation: ChartInfo;
@@ -18,6 +20,8 @@ export class PagingModel {
     public pageNumber: number = 0;
     public pageSize: number = 10;
     public totalRows: number = 0;
+    public totals: number = 0;
+    public showTotals: boolean = false;
     public data: Array<any> = new Array<any>();
     public HighChartsOptions: Options;
     public ChartInformation: ChartInfo;
@@ -30,6 +34,8 @@ export class PagingModel {
         this.pageNumber = pagingModel.pageNumber === undefined ? 0 : pagingModel.pageNumber;
         this.pageSize = pagingModel.pageSize === undefined ? 10 : pagingModel.pageSize;
         this.totalRows = pagingModel.totalRows === undefined ? 0 : pagingModel.totalRows;
+        this.totals = pagingModel.totals === undefined ? 0 : pagingModel.totals;
+        this.showTotals = pagingModel.showTotals === undefined ? false : pagingModel.showTotals;
         this.data = pagingModel.data;
         this.HighChartsOptions = pagingModel.HighChartsOptions;
         this.ChartInformation = pagingModel.ChartInformation;
