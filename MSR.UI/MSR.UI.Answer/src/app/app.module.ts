a2e29c2cf9b2a1028111df67bb3687c87e76005c
starsoftdev <derek.otieno.2020@gmail.com>
2022-12-12 02:10:20 -0500

    upgrade angular to v11 from v9

--- MSR.UI/MSR.UI.Answer/src/app/app.module.ts
+++ MSR.UI/MSR.UI.Answer/src/app/app.module.ts
@@ -1,34 +1,51 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { RouterModule, PreloadAllModules } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { RouterModule, PreloadAllModules } from "@angular/router";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ToastrModule } from "ngx-toastr";
 
-import { ROUTES } from './app.routes';
-import { CheckAllService } from './layout/utils/directives/check-all.service';
-import { AppComponent } from './app.component';
-import { ErrorComponent } from './pages/error/error.component';
-import { LoginService } from './pages/login/login.service';
-import { ResetpasswordService } from './pages/resetpassword/resetpassword.service';
-import { AppGuard } from './app.guard';
-import { AppInterceptor } from './app.interceptor';
-import { AppConfig } from './app.config';
-import { Globals } from './models/lib/globals';
-import { NotificationService } from './layout/navbar/notification.service';
-import { CommonGrid } from './models/lib/CommonGrid';
-import { CSVConverterService } from '../app/services/csvconverter.service';
-import { environment } from '../environments/environment';
+import { ROUTES } from "./app.routes";
+import { CheckAllService } from "./layout/utils/directives/check-all.service";
+import { AppComponent } from "./app.component";
+import { ErrorComponent } from "./pages/error/error.component";
+import { LoginService } from "./pages/login/login.service";
+import { ResetpasswordService } from "./pages/resetpassword/resetpassword.service";
+import { AppGuard } from "./app.guard";
+import { AppInterceptor } from "./app.interceptor";
+import { AppConfig } from "./app.config";
+import { Globals } from "./models/lib/globals";
+import { NotificationService } from "./layout/navbar/notification.service";
+import { CommonGrid } from "./models/lib/CommonGrid";
+import { CSVConverterService } from "../app/services/csvconverter.service";
+import { environment } from "../environments/environment";
 // import { GridComponent } from '../app/components/grid/grid.component';
 
-import * as $ from 'jquery';
+import * as $ from "jquery";
 import {
-  UserService, AccountService, API_BASE_URL, WorkflowService, WorkflowGroupService, CustomerService,
-  WorkflowStageService, LocationService, RoleService, WorkflowPendingApprovalService, PartService,
-  FileService, InvoiceService, WorkOrderService, TimezoneService, PurchaseOrderService, ProductService, ReportService,
-  SearchService, ProcedureService, DocumentService
-} from './services/api.client.generated';
+  UserService,
+  AccountService,
+  API_BASE_URL,
+  WorkflowService,
+  WorkflowGroupService,
+  CustomerService,
+  WorkflowStageService,
+  LocationService,
+  RoleService,
+  WorkflowPendingApprovalService,
+  PartService,
+  FileService,
+  InvoiceService,
+  WorkOrderService,
+  TimezoneService,
+  PurchaseOrderService,
+  ProductService,
+  ReportService,
+  SearchService,
+  ProcedureService,
+  DocumentService,
+} from "./services/api.client.generated";
 
 const APP_PROVIDERS = [
   CheckAllService,
@@ -39,15 +56,12 @@ const APP_PROVIDERS = [
   Globals,
   CommonGrid,
   CSVConverterService,
-  NotificationService
+  NotificationService,
 ];
 
 @NgModule({
   bootstrap: [AppComponent],
-  declarations: [
-    AppComponent,
-    ErrorComponent
-  ],
+  declarations: [AppComponent, ErrorComponent],
   exports: [],
   imports: [
     BrowserModule,
@@ -57,15 +71,16 @@ const APP_PROVIDERS = [
     ToastrModule.forRoot(),
     RouterModule.forRoot(ROUTES, {
       useHash: true,
-      preloadingStrategy: PreloadAllModules
-    })
+      preloadingStrategy: PreloadAllModules,
+      relativeLinkResolution: "legacy",
+    }),
   ],
   providers: [
     APP_PROVIDERS,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AppInterceptor,
-      multi: true
+      multi: true,
     },
     Globals,
     AccountService,
@@ -82,15 +97,21 @@ const APP_PROVIDERS = [
     WorkOrderService,
     {
       provide: API_BASE_URL,
-      useValue: environment.url
+      useValue: environment.url,
     },
     LocationService,
     RoleService,
-    PurchaseOrderService, CustomerService, ProductService, ReportService, SearchService, ProcedureService, DocumentService
-  ]
+    PurchaseOrderService,
+    CustomerService,
+    ProductService,
+    ReportService,
+    SearchService,
+    ProcedureService,
+    DocumentService,
+  ],
 })
 // { //we have this bse url set in the app.config that's why we define as ''
 //   provide: API_BASE_URL,
 //   useValue: environment.url
 // }
-export class AppModule { }
+export class AppModule {}
