b9a77e65d178665acf703d6f8534bb4e142d150b
Jared Mahan <jaredmahan@gmail.com>
2023-09-02 00:34:57 -0500

    fix: Remove the transmission workflow from the main action thread

--- MSR.Domain/Views/IntelWorkOrderMonitorView.cs
+++ MSR.Domain/Views/IntelWorkOrderMonitorView.cs
@@ -9,5 +9,6 @@ namespace MSR.Domain.Views
         public string UnitOfMeasure { get; set; }
         public string MeasurementType { get; set; }
         public string MeasurementValue { get; set; }
+        public string ControlValue { get; set; }
     }
 };
