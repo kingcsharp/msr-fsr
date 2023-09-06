ad1f0ee29bf5b5c8187cfad33fd0ae09c793aa68
Jared Mahan <jaredmahan@gmail.com>
2023-09-05 21:28:20 -0500

    Update IntelWorkOrderPartView.cs

--- MSR.Domain/Views/IntelWorkOrderPartView.cs
+++ MSR.Domain/Views/IntelWorkOrderPartView.cs
@@ -3,27 +3,27 @@ namespace MSR.Domain.Views
 {
 	public class IntelWorkOrderPartView
 	{
-        public int WorkOrderPartId { get; set; }
-        public int WorkOrderId { get; set; }
-        public string UnitNumber { get; set; }
-        public string? ThisDocumentGeneration { get; set; }
-        public String ScheduledShipDate { get; set; }
-        public string ResponsiblePartyEmail { get; set; }
-        public string PurchaseOrderNumber { get; set; }
-        public string ProductName { get; set; }
-        public string PartRevisionNumber { get; set; }
-        public string ManufacturingPlantCode { get; set; }
-        public string ManufacturerPartNumber { get; set; }
-        public string ManufacturerOrderNumber { get; set; }
-        public string ManufacturerNumber { get; set; }
-        public string ManufacturerName { get; set; }
-        public string LotCreatedDate { get; set; }
-        public string KitNumber { get; set; }
-        public string KitName { get; set; }
-        public int CycleCount { get; set; }
-        public string CustomerPartNumber { get; set; }
-        public string CustomerPartName { get; set; }
-        public string CustomerName { get; set; }
-        public string? ActualShipDate { get; set; }
-    }
+	        public int WorkOrderPartId { get; set; }
+	        public int WorkOrderId { get; set; }
+	        public string UnitNumber { get; set; }
+	        public string? ThisDocumentGeneration { get; set; }
+	        public String ScheduledShipDate { get; set; }
+	        public string ResponsiblePartyEmail { get; set; }
+	        public string PurchaseOrderNumber { get; set; }
+	        public string ProductName { get; set; }
+	        public string PartRevisionNumber { get; set; }
+	        public string ManufacturingPlantCode { get; set; }
+	        public string ManufacturerPartNumber { get; set; }
+	        public string ManufacturerOrderNumber { get; set; }
+	        public string ManufacturerNumber { get; set; }
+	        public string ManufacturerName { get; set; }
+	        public string LotCreatedDate { get; set; }
+	        public string KitNumber { get; set; }
+	        public string KitName { get; set; }
+	        public int CycleCount { get; set; }
+	        public string CustomerPartNumber { get; set; }
+	        public string CustomerPartName { get; set; }
+	        public string CustomerName { get; set; }
+	        public string? ActualShipDate { get; set; }
+    	}
 }
