0d30be94f043850d69f92c435898b6f5e31e6be9
Jared Mahan <jaredmahan@gmail.com>
2023-09-05 21:26:59 -0500

    Update XmlTransmissionLog.cs

--- MSR.Infrastructure/Resources/EntityFramework/Entities/XmlTransmissionLog.cs
+++ MSR.Infrastructure/Resources/EntityFramework/Entities/XmlTransmissionLog.cs
@@ -4,14 +4,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace MSR.Infrastructure.Resources.EntityFramework.Entities
 {
     [Table(nameof(XmlTransmissionLog))]
-    public class XmlTransmissionLog: Entity
-	{
+    public class XmlTransmissionLog: Entity 
+    {
         public string Result { get; set; }
         public DateTime SubmittedOn { get; set; }
         public string TransmissionDetail { get; set; }
         public int WorkOrderId { get; set; }
         public string XmlLink { get; set; }
-        
-	}
+    }
 }
 
