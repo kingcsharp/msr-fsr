709bfe4af41048d9b52b9b91860d41c026931aa1
JUSTIN BEUCK <justinbeuck@JUSTINs-MacBook-Pro.local>
2023-12-21 13:37:30 -0800

    updated dates to use moment to be inserted into usertimezone pipe

--- MSR.UI/MSR.UI.Answer/src/app/components/grid/grid.component.ts
+++ MSR.UI/MSR.UI.Answer/src/app/components/grid/grid.component.ts
@@ -26,6 +26,7 @@ import { EnumColumnType } from "../../models/enums/EnumColumnType";
 import { GridSaved } from "../../models/lib/GridSaved";
 import { ReportCubeService } from "../../pages/reports/reportcube.service";
 import * as Highcharts from "highcharts";
+import * as moment from "moment";
 import { ChartInfo } from "../../../app/models/lib/ChartInfo";
 import { CSVConverterService } from "../../services/csvconverter.service";
 import { LocaleSettings } from "primeng/calendar";
@@ -77,7 +78,7 @@ export class GridComponent implements OnInit {
     private cSVConverterService: CSVConverterService,
     private documentService: DocumentService,
     private xmlService: XmlService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.calendarLocalSettings = this.globals.getCalendarDefault();
@@ -149,7 +150,6 @@ export class GridComponent implements OnInit {
           lazyLoadEvent.filters
         );
     }
-
     this.getReport(this.data, this.reportInfo);
   }
 
@@ -165,7 +165,7 @@ export class GridComponent implements OnInit {
       );
       if (
         this.reportInfo.name.replace(/\s/g, "") +
-          this.reportInfo.subtitle.replace(/\s/g, "") ===
+        this.reportInfo.subtitle.replace(/\s/g, "") ===
         "PartsCycleCountsbyWorkOrderDate"
       ) {
         this.regnerateCharOptions(pagingModel.HighChartsOptions);
@@ -179,6 +179,7 @@ export class GridComponent implements OnInit {
 
   getReport(data: any, reportInfo?: ReportModel) {
     this.showCharts = false;
+
     if (reportInfo === undefined || reportInfo.apiEndPointURL === undefined) {
       this.showReport = false;
       this.gridData.length = 0;
@@ -198,7 +199,7 @@ export class GridComponent implements OnInit {
             this.gridData = responsePagingModel.data;
             if (
               reportInfo.name.replace(/\s/g, "") +
-                reportInfo.subtitle.replace(/\s/g, "") ===
+              reportInfo.subtitle.replace(/\s/g, "") ===
               "PartsCycleCountsbyWorkOrderDate"
             ) {
               this.regnerateCharOptions(responsePagingModel.HighChartsOptions);
@@ -240,6 +241,21 @@ export class GridComponent implements OnInit {
               }
             });
           }
+
+          if (responsePagingModel.data !== undefined && responsePagingModel.data.length !== 0) {
+            const convertUtcToDate = (utcDate: string): Date => {
+              const utcTime = moment.utc(utcDate).valueOf();
+              return new Date(utcTime);
+            };
+
+            responsePagingModel.data.forEach((object) => {
+              object.shipdate = convertUtcToDate(object.shipdate);
+              object.duedate = convertUtcToDate(object.duedate);
+              object.wocompleteddate = convertUtcToDate(object.wocompleteddate);
+              object.wocreationdate = convertUtcToDate(object.wocreationdate);
+            });
+          }
+
           this.filteredData = responsePagingModel.data;
           this.totalRows = responsePagingModel.totalRows;
           this.pagingModel.pageNumber = responsePagingModel.pageNumber;
