0c611e7aa68f5304634d77a069067a8d7edc0fc8
startsoftdev <derek.otieno.2020@gmail.com>
2024-01-24 09:37:09 -0500

    fixed display saved documents error

--- MSR.UI/MSR.UI.Answer/src/app/pages/procedures/template/template.component.ts
+++ MSR.UI/MSR.UI.Answer/src/app/pages/procedures/template/template.component.ts
@@ -137,10 +137,11 @@ export class TemplateComponent implements OnInit {
             label: s.name,
             value: s.id,
           }));
-
-          const referenceDocuments = [];
+          
+          this.documentsAvailable = documentsAvailable;
           if (documentsAvailable.length > 0 && (this.procedureStepTemplate as any)?.referenceDocumentIds?.length > 0) {
-            (this.procedureStepTemplate as any).referenceDocumentIds.forEach(Id => {
+            const referenceDocuments = [];
+            for (let Id of (this.procedureStepTemplate as any).referenceDocumentIds) {
               for (let doc of documentsAvailable) {
                 if (Id === parseInt(doc.value)) {
                   referenceDocuments.push({
@@ -150,11 +151,10 @@ export class TemplateComponent implements OnInit {
                   break;
                 }
               }
-            });
+            }
 
             this.referenceDocuments = referenceDocuments;
           }
-          this.documentsAvailable = documentsAvailable;
         })
       );
   }
