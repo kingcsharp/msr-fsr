3610db254fbf59090f1b038f9e6ee7f22d2effa8
JUSTIN BEUCK <justinbeuck@JUSTINs-MacBook-Pro.local>
2024-02-27 21:13:57 -0800

    updated ngif for display in reports

--- MSR.UI/MSR.UI.Answer/src/app/components/technical-data-label/technical-data-label.component.ts
+++ MSR.UI/MSR.UI.Answer/src/app/components/technical-data-label/technical-data-label.component.ts
@@ -1,9 +1,8 @@
 import { Component, Input, OnInit } from "@angular/core";
-import { DataLabel } from "./../../models/data-label-model";
 import {
   WorkOrderModel,
-  WorkOrderTaskMonitorModel,
 } from "../../services/api.client.generated";
+import { PackingListViewModel } from "../detailed-packing-list/detailed-packing-list-view-model";
 
 @Component({
   selector: "technical-data-label",
@@ -12,56 +11,14 @@ import {
 })
 export class TechnicalDataLabelComponent implements OnInit {
   @Input() WorkOrder: WorkOrderModel;
-  labels: Array<DataLabel> = new Array<DataLabel>();
 
-  constructor() {}
+  packingList: PackingListViewModel;
+
+  constructor() {
+    this.packingList = new PackingListViewModel();
+  }
 
   ngOnInit(): void {
-    this.WorkOrder.workOrderTasks.map((s) => {
-
-      if (
-        s.workOrderTaskMonitors === null ||
-        s.workOrderTaskMonitors === undefined
-      ) {
-        s.workOrderTaskMonitors = new Array<WorkOrderTaskMonitorModel>();
-      }
-    });
-
-    this.generateLabels();
-  }
-
-  generateLabels() {
-    this.WorkOrder.workOrderTasks.map((workOrderTask) => {
-      for (
-        let index = 0;
-        index < workOrderTask.workOrderTaskMonitors.length;
-        index++
-      ) {
-        let dataLabel = new DataLabel();
-        dataLabel.workOrderTaskMonitors = workOrderTask.workOrderTaskMonitors
-        dataLabel.CustomerPurchaseNumber =
-          this.WorkOrder.purchase.customerPurchaseNumber;
-        dataLabel.Date = this.WorkOrder.scheduledEndDate;
-        dataLabel.MonitorName =
-          workOrderTask.workOrderTaskMonitors[index].procedureMonitorId !== null
-            ? workOrderTask.workOrderTaskMonitors[index].procedureStepMonitor
-                ?.description
-            : workOrderTask.workOrderTaskMonitors[index]?.description;
-        dataLabel.PartName = this.WorkOrder.product?.part?.name;
-        dataLabel.PartNumber = this.WorkOrder.product?.part?.partNumber;
-        dataLabel.Requestee = workOrderTask.assignedToUser?.fullName;
-        dataLabel.TaskDescription =
-          workOrderTask.procedureStepId !== null
-            ? workOrderTask.procedureStep?.stepText
-            : workOrderTask.stepText;
-        this.labels.push(dataLabel);
-      }
-    });
-  }
-
-  checkMonitorType(label: DataLabel): boolean {
-    return label.workOrderTaskMonitors.some(
-      workOrder => workOrder.monitorTypeId === 2
-    );
+    this.packingList.populate(this.WorkOrder, this.WorkOrder.purchase);
   }
 }
