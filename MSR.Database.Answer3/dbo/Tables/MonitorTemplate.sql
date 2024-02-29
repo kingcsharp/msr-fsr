d5283f6f8d22332672c6796088a5fcc19694456c
JUSTIN BEUCK <justinbeuck@JUSTINs-MacBook-Pro.local>
2024-02-29 07:00:07 -0800

    added ANSRM-417 into code to get DisplayInReports

--- MSR.Database.Answer3/dbo/Tables/MonitorTemplate.sql
+++ MSR.Database.Answer3/dbo/Tables/MonitorTemplate.sql
@@ -13,6 +13,7 @@
     [FailAction]       VARCHAR (20)  NOT NULL,
     [SensorMappingId]  INT           NOT NULL,
     [SendNCREmail]     BIT           NULL,
+    [DisplayInReports] BIT           NULL,
     [CreatedBy]        INT           NOT NULL,
     [CreateOn]         DATETIME      NOT NULL,
     [LastUpdatedBy]    INT           NULL,
